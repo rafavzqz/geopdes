be5ab36a372be6aca65b83bc41bf1572d5a0d209
cdf <cdf@de73d896-90e3-4c87-98df-8390668dc151>
2010-12-03 00:01:48 +0000

    guscio della waveguide

---
+++ trunk/geopdes_elasticity/inst/multipatch/data_files/test_waveguide_3d_h_drchlt.m
@@ -0,0 +1,21 @@
+% TEST_WAVEGUIDE_3D_H_DRCHLT: data function for Dirichlet boundary condition.
+
+function h = test_waveguide_3d_h_drchlt (x, y, z, ind)
+
+  switch (ind)
+   case 3
+    h = cat(1, reshape (0*x, [1, size(x)]), ...
+            reshape (0*x+.5, [1, size(x)]), ...
+            reshape (0*x+2, [1, size(x)]));
+    case 4
+     h = cat(1, reshape (0*x, [1, size(x)]), ...
+             reshape (0*x, [1, size(x)]), ...
+             reshape (0*x, [1, size(x)]));
+    otherwise
+     h = cat(1, reshape (0*x, [1, size(x)]), ...
+             reshape (0*x, [1, size(x)]), ...
+             reshape (0*x, [1, size(x)]));
+  end
+
+end
+
