35707a45f542fb2d6b47574ece955717ef1670a5
cdf <cdf@de73d896-90e3-4c87-98df-8390668dc151>
2011-01-18 14:00:25 +0000

    w.g. elasticity

--- trunk/geopdes_elasticity/inst/multipatch/data_files/test_waveguide_3d_mp_h_drchlt.m
+++ trunk/geopdes_elasticity/inst/multipatch/data_files/test_waveguide_3d_mp_h_drchlt.m
@@ -1,14 +1,11 @@
 % TEST_WAVEGUIDE_3D_MP_H_DRCHLT: data function for Dirichlet boundary condition.
 
 function h = test_waveguide_3d_mp_h_drchlt (x, y, z, ind)
-
-  a = cos (pi/8);
-  b = sin (pi/8);
   switch (ind)
    case {1, 11, 12, 13, 14}
     h = cat(1, reshape (0*x, [1, size(x)]), ...
-            reshape ((a-1)*y-b*z, [1, size(x)]), ...
-            reshape (b*y+(a-1)*z, [1, size(x)]));
+            reshape (0*x+.5, [1, size(x)]), ...
+            reshape (0*x+.7, [1, size(x)]));
     case {2, 7, 8, 9, 10}
      h = cat(1, reshape (0*x, [1, size(x)]), ...
              reshape (0*x, [1, size(x)]), ...
