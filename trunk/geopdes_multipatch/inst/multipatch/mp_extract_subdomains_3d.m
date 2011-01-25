ba98326ee67a9c0e8aad3703c8e6d54379e63a4a
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2011-01-25 14:26:59 +0000

    Nuova versione di extract

---
+++ trunk/geopdes_multipatch/inst/multipatch/mp_extract_subdomains_3d.m
@@ -0,0 +1,106 @@
+% MP_EXTRACT_SUBDOMAINS_3D: create a multipatch geometry extracting a
+% set of subdomains from a gigen multipatch geometry.
+%
+% [new_geometry, new_interfaces, new_boundaries] = mp_extract_subdomains_3d (geometry, interfaces, boundaries, subdomains)
+%
+% INPUT :
+%
+%   geometry, interfaces, boundaries: initial multipatch geometry
+%   subdomains: list of subdomains to etract
+%
+% OUTPUT:
+%
+%   new_geometry, new_interfaces, new_boundaries: extracted multipatch geometry
+%
+% Copyright (C) 2010 Carlo de Falco, Rafael Vazquez
+% 
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with Octave; see the file COPYING.  If not, see
+% <http://www.gnu.org/licenses/>.
+
+
+function [new_geometry, new_interfaces, new_boundaries] = ...
+     mp_extract_subdomains_3d (geometry, interfaces, boundaries, subdomains)
+
+% Remove subdomains from the geometry structure
+npatch = numel (geometry);
+nsubdomains = numel (subdomains);
+
+new_subdomains = zeros (1, npatch);
+new_subdomains(subdomains) = 1:nsubdomains;
+
+new_geometry = geometry(subdomains);
+
+% Check which interfaces remain in the new geometry
+keep_int = ismember ([interfaces.patch1], subdomains) & ...
+           ismember ([interfaces.patch2], subdomains);
+new_num_1 = num2cell (new_subdomains([interfaces.patch1]));
+new_num_2 = num2cell (new_subdomains([interfaces.patch2]));
+new_interfaces = interfaces;
+[new_interfaces.patch1] = deal (new_num_1{:});
+[new_interfaces.patch2] = deal (new_num_2{:});
+new_interfaces = new_interfaces(keep_int);
+
+% Check which boundaries remain in the new geometry
+keep_bnd = [];
+nbnd     = numel (boundaries);
+new_boundaries = boundaries;
+for ibnd = 1:nbnd
+  keep_patches = ismember (boundaries(ibnd).patches, subdomains);
+  if (any (keep_patches));
+    keep_bnd = [keep_bnd, ibnd];
+    new_boundaries(ibnd).patches = new_subdomains (new_boundaries(ibnd).patches);
+    new_boundaries(ibnd).patches = new_boundaries(ibnd).patches(keep_patches);
+    new_boundaries(ibnd).nsides  = numel (new_boundaries(ibnd).patches);
+    new_boundaries(ibnd).faces   = new_boundaries(ibnd).faces(keep_patches);
+%    new_boundaries(ibnd).flag    = ones (1, new_boundaries(ibnd).nsides);
+%    new_boundaries(ibnd).ornt1   = ones (1, new_boundaries(ibnd).nsides);
+%    new_boundaries(ibnd).ornt2   = ones (1, new_boundaries(ibnd).nsides);
+  end
+end
+new_boundaries = new_boundaries(keep_bnd);
+nbnd   = numel (new_boundaries);
+
+int_2_bnd_1 = ismember ([interfaces.patch1], subdomains) & ...
+             ~ismember ([interfaces.patch2], subdomains);
+naddbnd = sum (int_2_bnd_1);
+added_bnd = nbnd+(1:naddbnd);
+if (naddbnd)
+  [new_boundaries(added_bnd).name]     = deal (interfaces(int_2_bnd_1).ref);
+  [new_boundaries(added_bnd).nsides]   = deal (1);
+  new_num  = num2cell (new_subdomains([interfaces(int_2_bnd_1).patch1]));
+  [new_boundaries(added_bnd).patches]  = deal (new_num{:});
+  [new_boundaries(added_bnd).faces]    = deal (interfaces(int_2_bnd_1).side1);
+  for iii = added_bnd
+    [new_boundaries(iii).flag]     = 1;
+    [new_boundaries(iii).ornt1]    = 1;
+    [new_boundaries(iii).ornt2]    = 1;
+  end
+end
+
+nbnd   = numel (new_boundaries);
+int_2_bnd_2 = ~ismember ([interfaces.patch1], subdomains) & ...
+               ismember ([interfaces.patch2], subdomains);
+naddbnd = sum (int_2_bnd_2);
+added_bnd = nbnd+(1:naddbnd);
+if (naddbnd)
+  [new_boundaries(added_bnd).name]     = deal (interfaces(int_2_bnd_2).ref);
+  [new_boundaries(added_bnd).nsides]   = deal (1);
+  new_num  = num2cell (new_subdomains([interfaces(int_2_bnd_2).patch2]));
+  [new_boundaries(added_bnd).patches]  = deal (new_num{:});
+  [new_boundaries(added_bnd).faces]    = deal (interfaces(int_2_bnd_2).side2);
+  [new_boundaries(added_bnd).flag]     = deal (interfaces(int_2_bnd_2).flag);
+  [new_boundaries(added_bnd).ornt1]    = deal (interfaces(int_2_bnd_2).ornt1);
+  [new_boundaries(added_bnd).ornt2]    = deal (interfaces(int_2_bnd_2).ornt2);
+end
+end
---
+++ trunk/geopdes_multipatch/inst/multipatch/mp_interface_subdomains_3d_vec.m
@@ -0,0 +1,99 @@
+% MP_INTERFACE_SUBDOMAINS_3D_VEC: a.
+%
+%   [glob_num, glob_ndof] = mp_interface_subdomains ();
+%
+% INPUT:
+%
+%  interfaces: structure with the information of the interfaces between patches (see mp_geo_read_file)
+%  sp:         structure representing the space of discrete functions (see sp_bspline_3d_phys)
+%
+% OUTPUT:
+%
+%  glob_num:  global numbering of the discrete basis functions
+%  glob_ndof: total number of degrees of freedom
+%
+% Copyright (C) 2010 Carlo de Falco, Rafael Vazquez
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+function gn_sub1_to_sub2 = ...
+  mp_interface_subdomains_3d_vec (bnd_sub1, bnd_sub2, sp1, sp2, gn1, gn2)
+
+  for ind1 = 1:numel (bnd_sub1)
+
+    ind2 = strcmp ({bnd_sub2.name}, bnd_sub1(ind1).name);
+    if (~any (ind2) || isempty (bnd_sub1(ind1).flag))
+      continue
+    end
+
+    iptc1 = bnd_sub1(ind1).patches;
+    face1 = bnd_sub1(ind1).faces;
+
+    iptc2 = bnd_sub2(ind2).patches;
+    face2 = bnd_sub2(ind2).faces;
+
+    bnd_dofs1_comp1 = reshape (sp1{iptc1}.boundary(face1).comp_dofs{1}, ...
+                  sp1{iptc1}.boundary(face1).ndof_dir(1,:));
+    bnd_dofs1_comp2 = reshape (sp1{iptc1}.boundary(face1).comp_dofs{2}, ...
+                  sp1{iptc1}.boundary(face1).ndof_dir(2,:));
+    bnd_dofs1_comp3 = reshape (sp1{iptc1}.boundary(face1).comp_dofs{3}, ...
+                  sp1{iptc1}.boundary(face1).ndof_dir(3,:));
+
+    bnd_dofs2_comp1 = reshape (sp2{iptc2}.boundary(face2).comp_dofs{1}, ...
+                  sp2{iptc2}.boundary(face2).ndof_dir(1,:));
+    bnd_dofs2_comp2 = reshape (sp2{iptc2}.boundary(face2).comp_dofs{2}, ...
+                  sp2{iptc2}.boundary(face2).ndof_dir(2,:));
+    bnd_dofs2_comp3 = reshape (sp2{iptc2}.boundary(face2).comp_dofs{3}, ...
+                  sp2{iptc2}.boundary(face2).ndof_dir(3,:));
+
+    if (bnd_sub1(ind1).flag == -1)
+      bnd_dofs1_comp1 = bnd_dofs1_comp1';
+      bnd_dofs1_comp2 = bnd_dofs1_comp2';
+      bnd_dofs1_comp3 = bnd_dofs1_comp3';
+    end
+    if (bnd_sub1(ind1).ornt1 == -1)
+      bnd_dofs1_comp1 = flipud (bnd_dofs1_comp1);
+      bnd_dofs1_comp2 = flipud (bnd_dofs1_comp2);
+      bnd_dofs1_comp3 = flipud (bnd_dofs1_comp3);
+    end
+    if (bnd_sub1(ind1).ornt2 == -1)
+      bnd_dofs1_comp1 = fliplr (bnd_dofs1_comp1);
+      bnd_dofs1_comp2 = fliplr (bnd_dofs1_comp2);
+      bnd_dofs1_comp3 = fliplr (bnd_dofs1_comp3);
+    end
+    if (bnd_sub2(ind2).flag == -1)
+      bnd_dofs2_comp1 = bnd_dofs2_comp1';
+      bnd_dofs2_comp2 = bnd_dofs2_comp2';
+      bnd_dofs2_comp3 = bnd_dofs2_comp3';
+    end
+    if (bnd_sub2(ind2).ornt1 == -1)
+      bnd_dofs2_comp1 = flipud (bnd_dofs2_comp1);
+      bnd_dofs2_comp2 = flipud (bnd_dofs2_comp2);
+      bnd_dofs2_comp3 = flipud (bnd_dofs2_comp3);
+    end
+    if (bnd_sub2(ind2).ornt2 == -1)
+      bnd_dofs2_comp1 = fliplr (bnd_dofs2_comp1);
+      bnd_dofs2_comp2 = fliplr (bnd_dofs2_comp2);
+      bnd_dofs2_comp3 = fliplr (bnd_dofs2_comp3);
+    end
+
+    bnd_dofs1 = [bnd_dofs1_comp1(:); bnd_dofs1_comp2(:); bnd_dofs1_comp3(:)];
+    bnd_dofs2 = [bnd_dofs2_comp1(:); bnd_dofs2_comp2(:); bnd_dofs2_comp3(:)];
+
+    gn_sub1_to_sub2{ind1,1} = gn1{iptc1}(bnd_dofs1);
+    gn_sub1_to_sub2{ind1,2} = gn2{iptc2}(bnd_dofs2);
+
+    end
+  end
+end
