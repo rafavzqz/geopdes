95c67c6d08e2b591ad34fdddc95d0990be6cfc96
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2011-03-18 12:52:54 +0000

    Maxwell eigenvalue problem, direct formulation

--- trunk/geopdes_maxwell/inst/examples/solve_maxwell_eig_2d.m
+++ trunk/geopdes_maxwell/inst/examples/solve_maxwell_eig_2d.m
@@ -1,14 +1,15 @@
-% SOLVE_MAXWELL_SRC_2D: Solve a 2d Maxwell source problem with a B-spline discretization.
+% SOLVE_MAXWELL_EIG_2D: Solve the 2d Maxwell eigenvalue problem with a B-spline discretization.
 %
 % Example to solve the problem
 %
-%    curl ( epsilon(x) curl (u)) + mu u = f    in Omega = F((0,1)^2)
-%              (epsilon(x) curl(u)) x n = g    on Gamma_N
-%                                 u x n = h    on Gamma_D
+%    curl (epsilon(x) curl (u)) = lambda (mu(x) u)   in Omega = F((0,1)^2)
+%       (epsilon(x) curl(u)) x n = 0                  on Gamma_N
+%                          u x n = 0                  on Gamma_D
 %
 % USAGE:
 %
-%  [geometry, msh, space, u] = solve_maxwell_src_2d (problem_data, method_data)
+%  [geometry, msh, space, eigv, eigf] = 
+%                  solve_maxwell_eig_2d (problem_data, method_data)
 %
 % INPUT:
 %
@@ -16,11 +17,8 @@
 %    - geo_name:     name of the file containing the geometry
 %    - nmnn_sides:   sides with Neumann boundary condition (may be empty)
 %    - drchlt_sides: sides with Dirichlet boundary condition
-%    - c_mass:       coefficient for the mass matrix (mu in the equation)
-%    - c_stiff:      coefficient for the stiffness matrix (epsilon in the equation)
-%    - f:            source term
-%    - g:            function for Neumann condition (if nmnn_sides is not empty)
-%    - h:            function for Dirichlet boundary condition
+%    - c_elec_perm:  electric permittivity (epsilon in the equation)
+%    - c_magn_perm:  magnetic permeability (mu in the equation)
 %
 %  method_data : a structure with discretization data. Its fields are:
 %    - degree:     degree of the spline functions.
@@ -33,9 +31,10 @@
 %  geometry: geometry structure (see geo_load)
 %  msh:      mesh structure (see msh_push_forward_2d)
 %  space:    space structure (see sp_bspline_curl_transform_2d)
-%  u:        the computed degrees of freedom
+%  eigv:     the computed eigenvalues
+%  eigf:     degrees of freedom of the associated eigenfunctions
 %
-% See also EX_MAXWELL_SRC_LSHAPED for an example.
+% See also EX_MAXWELL_EIG_SQUARE for an example
 %
 % Copyright (C) 2010, 2011 Rafael Vazquez
 %
@@ -52,8 +51,8 @@
 %    You should have received a copy of the GNU General Public License
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function [geometry, msh, sp, u] = ...
-              solve_maxwell_src_2d (problem_data, method_data)
+function [geometry, msh, sp, eigv, eigf] = ...
+              solve_maxwell_eig_2d (problem_data, method_data)
 
 % Extract the fields from the data structures into local variables
 data_names = fieldnames (problem_data);
@@ -65,7 +64,7 @@ for iopt  = 1:numel (data_names)
   eval ([data_names{iopt} sprintf('= method_data.(data_names{iopt});')]);
 end
 
-% Construct geometry structure 
+% Construct geometry structure
 geometry = geo_load (geo_name);
 
 [knots, zeta] = kntrefine (geometry.nurbs.knots, n_sub, degree, regularity);
@@ -84,36 +83,21 @@ sp = sp_bspline_curl_transform_2d (knots_u1, knots_u2, degree1, degree2, msh);
 x = squeeze (msh.geo_map(1,:,:));
 y = squeeze (msh.geo_map(2,:,:));
 
-epsilon = reshape (c_stiff (x, y), msh.nqn, msh.nel);
-mu      = reshape (c_mass (x, y), msh.nqn, msh.nel);
-fval    = reshape (f(x, y), 2, msh.nqn, msh.nel);
+epsilon = reshape (c_elec_perm (x, y), msh.nqn, msh.nel);
+mu      = reshape (c_magn_perm (x, y), msh.nqn, msh.nel);
 
 % Assemble the matrices
-stiff_mat = op_curlu_curlv_2d (sp, sp, msh, epsilon);
-mass_mat  = op_u_v (sp, sp, msh, mu);
-rhs       = op_f_v (sp, msh, fval);
+stiff_mat = op_curlu_curlv_2d (sp, sp, msh, 1./mu);
+mass_mat  = op_u_v (sp, sp, msh, epsilon);
 
-% Apply Neumann boundary conditions
-for iside = nmnn_sides
-  x = squeeze (msh.boundary(iside).geo_map(1,:,:));
-  y = squeeze (msh.boundary(iside).geo_map(2,:,:));
-  gval = reshape (g (x, y, iside), 2, msh.boundary(iside).nqn, msh.boundary(iside).nel);
-
-  rhs(sp.boundary(iside).dofs) = rhs(sp.boundary(iside).dofs) + ...
-    op_f_v (sp.boundary(iside), msh.boundary(iside), gval);
-end
-
-% Apply Dirichlet boundary conditions
-u = zeros (sp.ndof, 1);
-[u_drchlt, drchlt_dofs] = sp_drchlt_l2_proj_uxn_2d (sp, msh, h, drchlt_sides);
-u(drchlt_dofs) = u_drchlt;
-
+% Apply homogeneous Dirichlet boundary conditions
+drchlt_dofs = unique ([sp.boundary(drchlt_sides).dofs]);
 int_dofs = setdiff (1:sp.ndof, drchlt_dofs);
-rhs(int_dofs) = rhs(int_dofs) - stiff_mat(int_dofs, drchlt_dofs)*u_drchlt ...
-                              - mass_mat(int_dofs, drchlt_dofs)*u_drchlt;
 
-% Solve the linear system
-u(int_dofs) = (stiff_mat(int_dofs, int_dofs) + ...
-                mass_mat(int_dofs, int_dofs)) \ rhs(int_dofs);
+% Solve the eigenvalue problem
+eigf = zeros (sp.ndof, numel(int_dofs));
+[eigf(int_dofs, :), eigv] = ...
+    eig (full (stiff_mat(int_dofs, int_dofs)), full (mass_mat(int_dofs, int_dofs)));
+eigv = diag (eigv);
 
 end
