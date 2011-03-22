d138ec49329f05bf52f5ff391033745357a31e0d
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2011-03-22 10:27:37 +0000

    Minor things

--- trunk/geopdes_elasticity/inst/multipatch/ex_plane_strain_Lshaped_mp.m
+++ trunk/geopdes_elasticity/inst/multipatch/ex_plane_strain_Lshaped_mp.m
@@ -51,7 +51,7 @@ method_data.nquad      = [4 4];     % Points for the Gaussian quadrature rule
 output_file = 'plane_strain_Lshaped_mp_Deg3_Reg2_Sub5';
 
 vtk_pts = {linspace(0, 1, 20)', linspace(0, 1, 20)'};
-fprintf ('results being saved in: %s_displacement.pvd\n', output_file)
+fprintf ('results being saved in: %s_displacement.pvd\n \n', output_file)
 mp_sp_to_vtk_2d (u, space, geometry, gnum, vtk_pts, sprintf ('%s_displacement', output_file), 'displacement')
 
 % 4.2) COMPARISON WITH THE EXACT SOLUTION
