f818524582b0a67ace65195b81184f86aed34621
rafavzqz <rafavzqz@users.noreply.github.com>
2011-03-22 10:32:17 +0000

    Menu for the examples. Not finished

--- geopdes_multipatch/inst/examples/geopdes_multipatch_examples.m
+++ geopdes_multipatch/inst/examples/geopdes_multipatch_examples.m
@@ -25,78 +25,205 @@ while (iopt > 0)
            ['GeoPDEs_multipatch examples menu:\n', ...
             '---------------------------------\n', ...
             '\n', ...
-            '   (1) Examples in 2D: Poisson problem. \n \n',...
-            '   (2) Examples in 3D: Poisson problem. \n \n']);
+            '   (1) Poisson problem. \n \n',...
+            '   (2) Linear elasticity problems (geopdes_elasticity must be installed) \n \n',...
+            '   (3) Stokes problem (geopdes_fluid must be installed) \n \n',...
+            '   (4) Maxwell eigenvalue problem (geopdes_maxwell must be installed). \n \n']);
 
   iopt = input ('Please choose a number from above or press <Enter> to return: ');
   clc;
 
   if (iopt == 1)
-    clc;
-    fprintf (1, ...
-            ['GeoPDEs_multipatch examples menu: 2D examples\n', ...
-             '---------------------------------------------\n', ...
-             '\n', ...
-             '   2D Poisson problem in a multipatch domain. \n \n', ...
-             '   You can take a look at the script file: examples/ex_bspline_laplace_2d_mp.m \n \n', ...
-             '   You can solve with one of the following data files: \n \n', ...
-             '   - test_Lshaped_mp \n \n']);
-    fprintf ('Please write the name of the data file, including quotation marks: \n');
-    data_file = input ('[test_Lshaped_mp] ', 's');
-    if (~isempty (data_file))
-      eval(data_file);
-    else 
-      eval('test_Lshaped_mp');
-    end
-    vexa = do_example (1);
-    eval (vexa);
-fprintf (1, ...
-        ['\nYou may modify the data file to solve in the same geometry with rotated patches\n']);
-    input ('Press <Enter> to continue: ');
+    iopt2 = 1;
+    while (iopt2 > 0)
+      clc;
+      fprintf (1, ...
+        ['GeoPDEs_multipatch examples menu: Poisson problem \n', ...
+         '------------------------------------------------- \n', ...
+         '\n', ...
+         '2D examples \n \n', ...
+         '   (1) L-shaped domain, defined with 3 patches. \n \n',...
+         '3D examples \n \n', ...
+         '   (2) Unit cube, defined with 2 patches. \n \n',...
+         '   (3) Thick L-shaped domain, defined with 3 patches. \n \n']);
+
+      iopt2 = input ('Please choose a number from above or press <Enter> to return: ');
+      switch iopt2
+       case 1
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_LAPLACE_BSP_LSHAPED_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_laplace_bsp_Lshaped_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 2
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_LAPLACE_BSP_CUBE_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_laplace_bsp_cube_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 3
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_LAPLACE_BSP_THICK_L_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_laplace_bsp_thick_L_mp;
+        input ('Press <Enter> to continue: ');
+      end %switch
+    end %while iopt2>0
 
   elseif (iopt == 2)
-    clc;
-    fprintf (1, ...
-            ['GeoPDEs_multipatch examples menu: 3D examples\n', ...
-             '---------------------------------------------\n', ...
-             '\n', ...
-             '   3D Poisson problem in a multipatch domain. \n \n', ...
-             '   You can take a look at the script file: examples/ex_bspline_laplace_3d_mp.m \n \n', ...
-             '   You can solve with one of the following data files: \n \n', ...
-             '   - test_cube_mp \n',...
-             '   - test_thick_Lshaped_mp \n \n']);
-    fprintf ('Please write the name of the data file, including quotation marks: \n');
-    data_file = input ('[test_thick_Lshaped_mp] ', 's');
-    if (~isempty (data_file))
-      eval(data_file);
-    else 
-      eval('test_thick_Lshaped_mp');
+    if (~exist('ex_plane_strain_Lshaped_mp'))
+      fprintf(1, 'You must install geopdes_elasticity to run the examples\n\n');
+      iopt2 = -1;
+      input ('Press <Enter> to continue: ');
+    else
+      iopt2 = 1;
     end
-    vexa = do_example (2);
-    eval (vexa);
-    input ('Press <Enter> to continue: ');
+    while (iopt2 > 0)
+      clc;
+      fprintf (1, ...
+        ['GeoPDEs_multipatch examples menu: Linear elasticity problems \n', ...
+         '------------------------------------------------------------\n', ...
+         '\n', ...
+         '2D examples: plane strain \n \n', ...
+         '   (1) L-shaped domain, defined with 3 patches. \n \n',...
+         '3D examples \n \n', ...
+         '   (2) Unit cube, defined with 2 patches. \n \n']);
+
+      iopt2 = input ('Please choose a number from above or press <Enter> to return: ');
+
+      switch iopt2
+       case 1
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_PLANE_STRAIN_LSHAPED_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_plane_strain_Lshaped_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 2
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_LIN_ELAST_CUBE_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_lin_elast_cube_mp;
+        input ('Press <Enter> to continue: ');
+      end
+    end %while iopt2>0
+
+  elseif (iopt == 3)
+    if (~exist('ex_stokes_mp'))
+      fprintf(1, 'GUARDA IL FILE DEGLI ESEMPI \n\n');
+      fprintf(1, 'You must install geopdes_fluid to run the examples\n\n');
+      iopt2 = -1;
+      input ('Press <Enter> to continue: ');
+    else
+      iopt2 = 1;
+    end
+    while (iopt2 > 0)
+      clc;
+      fprintf (1, ...
+        ['GeoPDEs_multipatch examples menu: Stokes problem \n', ...
+         '------------------------------------------------ \n', ...
+         '\n', ...
+         '2D examples \n \n', ...
+         '   (1) NON HO ANCORA FINITO \n \n',...
+         '3D examples \n \n', ...
+         '   (2) NON HO ANCORA FINITO \n \n']);
+
+      iopt2 = input ('Please choose a number from above or press <Enter> to return: ');
+    end %while iopt2>0
+
+
+  elseif (iopt == 4)
+    if (~exist('ex_maxwell_eig_cube_mp'))
+      fprintf(1, 'You must install geopdes_maxwell to run the examples\n\n');
+      iopt2 = -1;
+      input ('Press <Enter> to continue: ');
+    else
+      iopt2 = 1;
+    end
+    while (iopt2 > 0)
+      clc;
+      fprintf (1, ...
+        ['GeoPDEs_multipatch examples menu: Maxwell eigenvalue problem \n', ...
+         '------------------------------------------------------------ \n', ...
+         '\n', ...
+         '2D examples \n \n', ...
+         '   (1) L-shaped domain, defined with 3 patches. \n',...
+         '   (2) L-shaped domain, defined with 3 patches. Mixed formulation. \n \n',...
+         '3D examples \n \n', ...
+         '   (3) Unit cube, defined with 2 patches. \n',...
+         '   (4) Unit cube, defined with 2 patches. Mixed formulation.\n',...
+         '   (5) Thick L-shaped domain, defined with 3 patches. \n',...
+         '   (6) Thick L-shaped domain, defined with 3 patches. Mixed formulation.\n',...
+         '   (7) Fichera corner, defined with 7 patches. \n',...
+         '   (8) Fichera corner, defined with 7 patches. Mixed formulation.\n \n']);
+
+      iopt2 = input ('Please choose a number from above or press <Enter> to return: ');
+
+
+      switch iopt2
+       case 1
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_MAXWELL_EIG_LSHAPED_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_maxwell_eig_Lshaped_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 2
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_MAXWELL_EIG_MIXED1_LSHAPED_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_maxwell_eig_mixed1_Lshaped_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 3
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_MAXWELL_EIG_CUBE_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_maxwell_eig_cube_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 4
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_MAXWELL_EIG_MIXED1_CUBE_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_maxwell_eig_mixed1_cube_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 5
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_MAXWELL_EIG_THICK_L_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_maxwell_eig_thick_L_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 6
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_MAXWELL_EIG_MIXED1_THICK_L_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_maxwell_eig_mixed1_thick_L_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 7
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_MAXWELL_EIG_FICHERA_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_maxwell_eig_fichera_mp;
+        input ('Press <Enter> to continue: ');
+
+       case 8
+        clc;
+        fprintf (1, 'You can have a look at the source file: EX_MAXWELL_EIG_MIXED1_FICHERA_MP \n \n');
+        fprintf (1, 'You may also modify the file to solve in the same geometry with rotated patches\n \n');
+        ex_maxwell_eig_mixed1_fichera_mp;
+        input ('Press <Enter> to continue: ');
+
+    end %while iopt2>0
 
   end
 
 end %# while (iopt > 0)
 
-end
-
-function vexa = do_example (number)
-
-switch (number)
- case 1
-  filename = 'ex_bspline_laplace_2d_mp.m';
- case 2
-  filename = 'ex_bspline_laplace_3d_mp.m';
-end
-
-fid = fopen (filename);
-vexa = '';
-l = fgets (fid);
-while (l ~= -1)
-  vexa = cat (2, vexa, l);
-  l = fgets (fid);
-end
 end
 
