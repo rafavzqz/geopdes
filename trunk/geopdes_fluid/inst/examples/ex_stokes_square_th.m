3951a345900d8a262bef59f96e1a6f81d5ac9556
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2011-03-31 08:46:33 +0000

    Examples to test

--- trunk/geopdes_fluid/inst/examples/ex_stokes_square_th.m
+++ trunk/geopdes_fluid/inst/examples/ex_stokes_square_th.m
@@ -1,4 +1,4 @@
-% EX_STOKES_SQUARE: solve the Stokes problem in the unit square.
+% EX_STOKES_SQUARE_TH: solve the Stokes problem in the unit square with generalized Taylor-Hood elements.
 
 % 1) PHYSICAL DATA OF THE PROBLEM
 clear problem_data  
@@ -49,7 +49,7 @@ problem_data.gradvelex = @test_stokes_square_graduex;
 
 % 2) CHOICE OF THE DISCRETIZATION PARAMETERS
 clear method_data
-method_data.element_name = 'rt';   % Element type for discretization
+method_data.element_name = 'th';   % Element type for discretization
 method_data.degree       = [ 3  3];  % Degree of the splines
 method_data.regularity   = [ 2  2];  % Regularity of the splines
 method_data.nsub         = [10 10];  % Number of subdivisions
@@ -66,8 +66,8 @@ error_l2_p = sp_l2_error (space_p, msh, press, problem_data.pressex)
    sp_h1_error (space_v, msh, vel, problem_data.velex, problem_data.gradvelex)
 
 
-% 4.1) EXPORT TO PARAVIEW
-output_file = 'SQUARE_RT_Reg2_Sub10'
+% 4.2) EXPORT TO PARAVIEW
+output_file = 'SQUARE_TH_Deg3_Reg2_Sub10'
 
 vtk_pts = {linspace(0, 1, 20)', linspace(0, 1, 20)'};
 sp_to_vtk_2d (press, space_p, geometry, vtk_pts, output_file, 'press')
