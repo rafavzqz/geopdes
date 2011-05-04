3c8f44fb85d45f8b00c48424d93691a32de61fe8
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2011-05-04 15:57:00 +0000

    Removed sprintf

--- trunk/geopdes_multipatch/inst/multipatch/mp_solve_laplace_3d.m
+++ trunk/geopdes_multipatch/inst/multipatch/mp_solve_laplace_3d.m
@@ -61,11 +61,11 @@ function [geometry, msh, sp, u, gnum] = ...
 % Extract the fields from the data structures into local variables
 data_names = fieldnames (problem_data);
 for iopt  = 1:numel (data_names)
-  eval ([data_names{iopt} sprintf('= problem_data.(data_names{iopt});')]);
+  eval ([data_names{iopt} '= problem_data.(data_names{iopt});']);
 end
 data_names = fieldnames (method_data);
 for iopt  = 1:numel (data_names)
-  eval ([data_names{iopt} sprintf('= method_data.(data_names{iopt});')]);
+  eval ([data_names{iopt} '= method_data.(data_names{iopt});']);
 end
 
 % Construct geometry structure, and information for interfaces and boundaries
