d435af889a9ef6cef9bfce3fc18ecf91091bff4d
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2011-12-21 11:30:49 +0000

    Fixed bug when nsh is not constant

--- trunk/geopdes_elasticity/inst/operators/op_su_ev.m
+++ trunk/geopdes_elasticity/inst/operators/op_su_ev.m
@@ -83,11 +83,12 @@ function varargout = op_su_ev (spu, spv, msh, lambda, mu)
   end
 
   if (nargout == 1)
-    varargout{1} = sparse (rows, cols, values, spv.ndof, spu.ndof);
+    varargout{1} = sparse (rows(1:ncounter), cols(1:ncounter), ...
+                           values(1:ncounter), spv.ndof, spu.ndof);
   elseif (nargout == 3)
-    varargout{1} = rows;
-    varargout{2} = cols;
-    varargout{3} = values;
+    varargout{1} = rows(1:ncounter);
+    varargout{2} = cols(1:ncounter);
+    varargout{3} = values(1:ncounter);
   else
     error ('op_su_ev: wrong number of output arguments')
   end
