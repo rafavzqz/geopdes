17e06778d2b9a943dbfc26935134e3567c3ddc44
rafavzqz <rafavzqz@users.noreply.github.com>
2012-11-02 17:12:02 +0000

    Prepared for new release

--- geopdes_base/inst/space/sp_eval_msh.m
+++ geopdes_base/inst/space/sp_eval_msh.m
@@ -29,7 +29,7 @@
 %    You should have received a copy of the GNU General Public License
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function [eu, F] = sp_eval_msh (u, space, msh);
+function [eu, F] = sp_eval_msh (u, space, msh)
 
   ndim = numel (msh.qn);
 
