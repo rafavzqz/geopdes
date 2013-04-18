e7d498e88dc8c9d662dc31512b76dfb165fb19e8
rafavzqz <rafavzqz@users.noreply.github.com>
2013-04-18 14:03:50 +0000

    Added the second derivative for NURBS

--- geopdes_base/inst/space/@sp_nurbs_2d/sp_evaluate_col_param.m
+++ geopdes_base/inst/space/@sp_nurbs_2d/sp_evaluate_col_param.m
@@ -15,6 +15,7 @@
 %           ------------+-----------------+----------------------------------
 %            value      |      true       |  compute shape_functions
 %            gradient   |      false      |  compute shape_function_gradients
+%            hessian    |      false      |  compute shape_function_hessians
 %
 % OUTPUT:
 %
@@ -31,9 +32,12 @@
 %    shape_functions (msh_col.nqn x nsh_max x msh_col.nel)  basis functions evaluated at each quadrature node in each element
 %    shape_function_gradients
 %                (2 x msh_col.nqn x nsh_max x msh_col.nel)  basis function gradients evaluated at each quadrature node in each element
+%    shape_function_hessians
+%            (2 x 2 x msh_col.nqn x nsh_max x msh_col.nel)  basis function hessians evaluated at each quadrature node in each element
 %
 % Copyright (C) 2009, 2010, 2011 Carlo de Falco
-% Copyright (C) 2011 Rafael Vazquez
+% Copyright (C) 2011, 2013 Rafael Vazquez
+% Copyright (C) 2013 Marco Pingaro
 %
 %    This program is free software: you can redistribute it and/or modify
 %    it under the terms of the GNU General Public License as published by
@@ -52,6 +56,7 @@ function [sp, elem_list] = sp_evaluate_col_param (space, msh, varargin)
 
 value = true;
 gradient = false;
+hessian = false;
 if (~isempty (varargin))
   if (~rem (length (varargin), 2) == 0)
     error ('sp_evaluate_col_param: options must be passed in the [option, value] format');
@@ -61,6 +66,8 @@ if (~isempty (varargin))
       value = varargin {ii+1};
     elseif (strcmpi (varargin {ii}, 'gradient'))
       gradient = varargin {ii+1};
+    elseif (strcmpi (varargin {ii}, 'hessian'))
+      hessian = varargin {ii+1};
     else
       error ('sp_evaluate_col_param: unknown option %s', varargin {ii});
     end
@@ -107,15 +114,15 @@ shp_v = reshape (shp_v, msh.nqn, space.nsh_max, msh.nel);
 % Multiply each function by the weight and compute the denominator
 W = space.weights (connectivity);
 W = repmat (reshape (W, 1, space.nsh_max, msh.nel), [msh.nqn, 1, 1]);
-shape_functions = W.* shp_u .* shp_v ;
-D = repmat (reshape (sum (shape_functions, 2), msh.nqn, 1, msh.nel), [1, space.nsh_max, 1]);
-shape_functions = shape_functions ./ D;
+shape_fun = W.* shp_u .* shp_v ;
+D = repmat (reshape (sum (shape_fun, 2), msh.nqn, 1, msh.nel), [1, space.nsh_max, 1]);
+shape_functions = shape_fun ./ D;
 
 if (value)
   sp.shape_functions = shape_functions;
 end
 
-if (gradient)
+if (gradient || hessian)
   shg_u = reshape (spu.shape_function_gradients(:,:,msh.colnum), ...
                    msh.nqn_dir(1), 1, spu.nsh_max, 1, 1);  %% one column only
   shg_u = repmat  (shg_u, [1, msh.nqn_dir(2), 1, spv.nsh_max, msh.nel]);
@@ -138,9 +145,42 @@ if (gradient)
   sp.shape_function_gradients = reshape (shape_fun_grads, ...
                                 2, msh.nqn, sp.nsh_max, msh.nel);
 
-  clear shg_u shg_v shape_fun_grads Bu Bv Du Dv
+  if (hessian && isfield (msh, 'geo_map_der2'))
+      shh_uu = reshape (spu.shape_function_hessians(:,:,msh.colnum), msh.nqn_dir(1), 1, spu.nsh_max, 1, 1);
+      shh_uu = repmat  (shh_uu, [1, msh.nqn_dir(2), 1, spv.nsh_max, msh.nel]);
+      shh_uu = reshape (shh_uu, msh.nqn, sp.nsh_max, msh.nel);
+      
+      shh_vv = reshape (spv.shape_function_hessians, 1, msh.nqn_dir(2), 1, spv.nsh_max, msh.nel);
+      shh_vv = repmat  (shh_vv, [msh.nqn_dir(1), 1, spu.nsh_max, 1, 1]);
+      shh_vv = reshape (shh_vv, msh.nqn, sp.nsh_max, msh.nel);
+
+      Buu  = W.* shh_uu.* shp_v ; 
+      Bvv  = W.* shp_u .* shh_vv;
+      Buv  = W.* shg_u .* shg_v ;
+    
+      Duu = repmat (reshape (sum (Buu, 2), msh.nqn, 1, msh.nel_dir(2)), [1, sp.nsh_max, 1]);
+      Dvv = repmat (reshape (sum (Bvv, 2), msh.nqn, 1, msh.nel_dir(2)), [1, sp.nsh_max, 1]);
+      Duv = repmat (reshape (sum (Buv, 2), msh.nqn, 1, msh.nel_dir(2)), [1, sp.nsh_max, 1]);
+    
+      % Nuu
+      shape_function_hessians(1,1,:,:,:) = Buu./D - (2.*Bu.*Du + shape_fun .* Duu)./(D.*D)...
+          + 2 .* shape_fun .* Du .* Du./(D.*D.*D);
+      % Nuv & Nvu
+      shape_function_hessians(1,2,:,:,:) = Buv./D - (Bu.*Dv + Bv.*Du + shape_fun.*Duv)./(D.*D)...
+          + 2 .* shape_fun .* Du .* Dv ./(D.*D.*D);
+      shape_function_hessians(2,1,:,:,:) = shape_function_hessians(1,2,:,:,:);
+      % Nvv
+      shape_function_hessians(2,2,:,:,:) = Bvv./D - (2.*Bv.*Dv + shape_fun .* Dvv)./(D.*D)...
+          + 2 .* shape_fun .* Dv .* Dv./(D.*D.*D);
+    
+      sp.shape_function_hessians = shape_function_hessians;  
+  end
+
 end
 
+clear shh_uu shh_vv shape_function_hessians
+clear shg_u shg_v shape_fun_grads Bu Bv Du Dv  
+clear Buu Bvv Buv Duu Dvv Duv 
 clear shp_u shp_v shape_functions
 
 end
