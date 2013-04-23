0124648c3444d91cde7850b0ece655b664e84144
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2013-04-23 13:11:24 +0000

    Added the functions for the bilaplacian problem

--- trunk/geopdes_base/inst/space/@sp_bspline_2d/sp_bspline_2d.m
+++ trunk/geopdes_base/inst/space/@sp_bspline_2d/sp_bspline_2d.m
@@ -78,6 +78,11 @@ function sp = sp_bspline_2d (knots, degree, msh)
     boundary(2).dofs = sub2ind (sp.ndof_dir, mcp*ones(1,ncp), 1:ncp);
     boundary(3).dofs = sub2ind (sp.ndof_dir, 1:mcp, ones(1,mcp));
     boundary(4).dofs = sub2ind (sp.ndof_dir, 1:mcp, ncp*ones(1,mcp));
+
+    boundary(1).adjacent_dofs = sub2ind ([mcp, ncp], 2*ones(1,ncp), 1:ncp);
+    boundary(2).adjacent_dofs = sub2ind ([mcp, ncp], (mcp-1)*ones(1,ncp), 1:ncp);
+    boundary(3).adjacent_dofs = sub2ind ([mcp, ncp], 1:mcp, 2*ones(1,mcp));
+    boundary(4).adjacent_dofs = sub2ind ([mcp, ncp], 1:mcp,(ncp-1)*ones(1,mcp));
     
     sp.boundary = boundary;
   else
