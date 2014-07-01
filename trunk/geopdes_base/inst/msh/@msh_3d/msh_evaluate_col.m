062dd89a3b0ff23b13184328dac65b02add39da2
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2014-07-01 16:18:51 +0000

    Fixed bug for plotting

--- trunk/geopdes_base/inst/msh/@msh_3d/msh_evaluate_col.m
+++ trunk/geopdes_base/inst/msh/@msh_3d/msh_evaluate_col.m
@@ -130,7 +130,7 @@ function msh_col = msh_evaluate_col (msh, colnum, varargin)
     msh_col.jacdet = msh.jacdet(:,msh_col.elem_list);
   end
 
-  if (~isempty(msh.quad_weights) && ~isempty(msh_col.jacdet))
+  if (~isempty(msh_col.quad_weights) && ~isempty(msh_col.jacdet))
     msh_col.element_size = (sum (msh_col.quad_weights .* ...
                              abs (msh_col.jacdet), 1)).^(1/3);
   end
