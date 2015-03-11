01da27e3f5707d68a503e3dacfa9be11893f1c98
rafavzqz <rafavzqz@users.noreply.github.com>
2015-03-11 11:09:15 +0000

    Neumann condition in 1d

--- geopdes_base/inst/solve/solve_laplace.m
+++ geopdes_base/inst/solve/solve_laplace.m
@@ -85,16 +85,26 @@ rhs       = op_f_v_tp (space, msh, f);
 
 % Apply Neumann boundary conditions
 for iside = nmnn_sides
-  msh_side = msh_eval_boundary_side (msh, iside);
-  sp_side  = sp_eval_boundary_side (space, msh_side);
+  if (msh.ndim > 1)
+    msh_side = msh_eval_boundary_side (msh, iside);
+    sp_side  = sp_eval_boundary_side (space, msh_side);
 
-  x = cell (msh_side.rdim, 1);
-  for idim = 1:msh_side.rdim
-    x{idim} = squeeze (msh_side.geo_map(idim,:,:));
+    x = cell (msh_side.rdim, 1);
+    for idim = 1:msh_side.rdim
+      x{idim} = squeeze (msh_side.geo_map(idim,:,:));
+    end
+    gval = reshape (g (x{:}, iside), msh_side.nqn, msh_side.nel);
+
+    rhs(sp_side.dofs) = rhs(sp_side.dofs) + op_f_v (sp_side, msh_side, gval);
+  else
+    if (iside == 1)
+      x = msh.breaks{1}(1);
+    else
+      x = msh.breaks{1}(end);
+    end
+    sp_side = space.boundary(iside);
+    rhs(sp_side.dofs) = rhs(sp_side.dofs) + g(x,iside);
   end
-  gval = reshape (g (x{:}, iside), msh_side.nqn, msh_side.nel);
-
-  rhs(sp_side.dofs) = rhs(sp_side.dofs) + op_f_v (sp_side, msh_side, gval);
 end
 
 % Apply Dirichlet boundary conditions
