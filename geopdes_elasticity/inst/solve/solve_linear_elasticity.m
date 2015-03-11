84bcee89011c9631dada175f19a75a83f2a2bf1a
rafavzqz <rafavzqz@users.noreply.github.com>
2015-03-11 17:18:13 +0000

    Fixed symmetry condition

--- geopdes_elasticity/inst/solve/solve_linear_elasticity.m
+++ geopdes_elasticity/inst/solve/solve_linear_elasticity.m
@@ -139,15 +139,20 @@ for iside = symm_sides
   for idim = 1:msh.rdim
     normal_comp(idim,:) = reshape (msh_side.normal(idim,:,:), 1, msh_side.nqn*msh_side.nel);
   end
-  
+
+  parallel_to_axes = false;
   for ind = 1:msh.rdim
     ind2 = setdiff (1:msh.rdim, ind);
     if (all (all (abs (normal_comp(ind2,:)) < 1e-10)))
       symm_dofs = union (symm_dofs, sp.boundary(iside).comp_dofs{ind});
+      parallel_to_axes = true;
       break
     end
+  end
+  if (~parallel_to_axes)
     error ('solve_linear_elasticity: We have only implemented the symmetry condition for boundaries parallel to the axes')
   end
+
 end
 
 % Apply Dirichlet boundary conditions
