ae2a415f24eb8db34f4a22cf72ad8fda2c77bb1a
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2015-03-13 15:46:56 +0000

    Changed function name

--- branches/manifolds/geopdes_base/inst/obsolete/solve_laplace_eig_2d.m
+++ branches/manifolds/geopdes_base/inst/obsolete/solve_laplace_eig_2d.m
@@ -1,6 +1,6 @@
 % -*- INTERNAL UNDOCUMENTED FUNCTION -*-
 function [geometry, msh, space, lambda, u] = ...
-              solve_laplace_eig (problem_data, method_data)
+              solve_laplace_eig_2d (problem_data, method_data)
 
 warning ('Function SOLVE_LAPLACE_EIG_2D is obsolete. Using SOLVE_LAPLACE_EIG instead')
 
