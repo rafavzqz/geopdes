cdc5d6e8694f031ea2b18040a5a76238d995924d
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2015-07-07 13:56:53 +0000

    Easier computation of boundary integrals

--- branches/manifolds/geopdes_elasticity/inst/solve/solve_linear_elasticity.m
+++ branches/manifolds/geopdes_elasticity/inst/solve/solve_linear_elasticity.m
@@ -103,15 +103,10 @@ rhs    = op_f_v_tp (sp, msh, f);
 
 % Apply Neumann boundary conditions
 for iside = nmnn_sides
-  msh_side = msh_eval_boundary_side (msh, iside);
-  sp_side  = sp_eval_boundary_side (sp, msh_side);
-
-  x = cell (msh_side.rdim, 1);
-  for idim = 1:msh_side.rdim
-    x{idim} = squeeze (msh_side.geo_map(idim,:,:));
-  end
-  gval = reshape (g (x{:}, iside), sp.ncomp, msh_side.nqn, msh_side.nel);
-  rhs(sp.boundary(iside).dofs) = rhs(sp.boundary(iside).dofs) + op_f_v (sp_side, msh_side, gval);
+% Restrict the function handle to the specified side, in any dimension, gside = @(x,y) g(x,y,iside)
+  gside = @(varargin) g(varargin{:},iside);
+  dofs = sp.boundary(iside).dofs;
+  rhs(dofs) = rhs(dofs) + op_f_v_tp (sp.boundary(iside), msh.boundary(iside), gside);
 end
 
 % Apply pressure conditions
