68512be196d001b6aafbbe58d96c304cd88b745d
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2015-10-16 11:06:48 +0000

    Some fixes for rdim and derivatives

--- branches/light_version/geopdes_base/inst/examples/geometry_files/ring_polar_map_der.m
+++ branches/light_version/geopdes_base/inst/examples/geometry_files/ring_polar_map_der.m
@@ -1,6 +1,6 @@
 % RING_POLAR_MAP_DER: Jacobian of the parameterization given in ring_polar_map.
 
-function jac = ring_polar_map_der (pts)
+function varargout = ring_polar_map_der (pts)
 
   if (iscell (pts))
     u = reshape (repmat (pts{1}(:), 1, numel (pts{2})), 1, []);
@@ -14,5 +14,14 @@ function jac = ring_polar_map_der (pts)
   jac(2,1,:) =  sin (pi.*v/2);
   jac(1,2,:) = -pi*(u+1) .* sin (pi.*v/2)/2;
   jac(2,2,:) =  pi*(u+1) .* cos (pi.*v/2)/2;
+  
+  if (nargout == 1)
+    varargout{1} = jac;
+  elseif (nargout == 2)
+    F = ring_polar_map (pts);
+    varargout{1} = F;
+    varargout{2} = jac;
+  end
+  
 end
 
