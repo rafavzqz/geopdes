e590c1688209af8460e5f629ffcc67d29deade3e
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2015-10-22 14:59:25 +0000

    Second working version

--- branches/light_version/geopdes_base/inst/space/sp_integral_preserving_transform.m
+++ branches/light_version/geopdes_base/inst/space/sp_integral_preserving_transform.m
@@ -3,6 +3,10 @@ function sp = sp_integral_preserving_transform (sp, msh, value)
   if (value)
     jacdet = reshape (geopdes_det__ (msh.geo_map_jac), msh.nqn, 1, msh.nel);
     sp.shape_functions = bsxfun (@rdivide, sp.shape_functions, jacdet);
+    sp.shape_functions = sp.shape_functions;
+    if (isfield (msh, 'side_number'))
+      sp.shape_functions = sp.shape_functions * (-1)^msh.side_number;
+    end
   end
   
   if (isfield (sp, 'shape_function_gradients'))
