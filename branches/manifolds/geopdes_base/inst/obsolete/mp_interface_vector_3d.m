bf65c25451bd4d1febffff134424e642e006f382
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2015-11-03 13:18:51 +0000

    Added colon

--- branches/manifolds/geopdes_base/inst/obsolete/mp_interface_vector_3d.m
+++ branches/manifolds/geopdes_base/inst/obsolete/mp_interface_vector_3d.m
@@ -3,4 +3,4 @@ function [glob_num, glob_ndof] = mp_interface_vector_3d (interfaces, sp)
 
 warning ('Function MP_INTERFACE_VECTOR_3D is obsolete. Using MP_INTERFACE_VECTOR instead')
 
-[glob_num, glob_ndof] = mp_interface_vector (interfaces, sp)
+[glob_num, glob_ndof] = mp_interface_vector (interfaces, sp);
