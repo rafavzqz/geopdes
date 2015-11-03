0b7415150149228afe358f6a590cfc5d8a385750
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2015-11-03 13:21:37 +0000

    Added colon

--- branches/manifolds/geopdes_maxwell/inst/obsolete/mp_interface_hcurl_2d.m
+++ branches/manifolds/geopdes_maxwell/inst/obsolete/mp_interface_hcurl_2d.m
@@ -3,4 +3,4 @@ function [glob_num, glob_ndof, dofs_ornt] = mp_interface_hcurl_2d (interfaces, s
 
 warning ('Function MP_INTERFACE_HCURL_2D is obsolete. Using MP_INTERFACE_HCURL instead')
 
-[glob_num, glob_ndof, dofs_ornt] = mp_interface_hcurl (interfaces, sp)
+[glob_num, glob_ndof, dofs_ornt] = mp_interface_hcurl (interfaces, sp);
