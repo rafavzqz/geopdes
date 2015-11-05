939cd088781a18703fbce72c2ea9c8cff133a593
rafavzqz <rafavzqz@users.noreply.github.com>
2015-11-05 16:28:15 +0000

    Output is already a struct

--- geopdes_base/inst/space/sp_weak_drchlt_bc.m
+++ geopdes_base/inst/space/sp_weak_drchlt_bc.m
@@ -58,7 +58,7 @@ function [A, rhs] = sp_weak_drchlt_bc (space, msh, bnd_sides, bnd_func, coeff, C
     msh_side_from_interior = msh_boundary_side_from_interior (msh, iside);
 
     sp_bnd = space.constructor (msh_side_from_interior);
-    sp_bnd = struct (sp_precompute (sp_bnd, msh_side_from_interior, 'value', true, 'gradient', true));
+    sp_bnd = sp_precompute (sp_bnd, msh_side_from_interior, 'value', true, 'gradient', true);
 
     for idim = 1:msh.rdim
       x{idim} = reshape (msh_side.geo_map(idim,:,:), msh_side.nqn, msh_side.nel);
