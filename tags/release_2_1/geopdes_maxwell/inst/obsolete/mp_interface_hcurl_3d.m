709cd25e7cb226a19b43e1db34137ea5f62cba5a
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2015-11-06 11:21:08 +0000

    Added colon

--- tags/release_2_1/geopdes_maxwell/inst/obsolete/mp_interface_hcurl_3d.m
+++ tags/release_2_1/geopdes_maxwell/inst/obsolete/mp_interface_hcurl_3d.m
@@ -3,4 +3,4 @@ function [glob_num, glob_ndof, dofs_ornt] = mp_interface_hcurl_3d (interfaces, s
 
 warning ('Function MP_INTERFACE_HCURL_3D is obsolete. Using MP_INTERFACE_HCURL instead')
 
-[glob_num, glob_ndof, dofs_ornt] = mp_interface_hcurl (interfaces, sp)
+[glob_num, glob_ndof, dofs_ornt] = mp_interface_hcurl (interfaces, sp);
