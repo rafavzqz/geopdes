2e9b780eee4ae0df41aa6b29e64ea8ea94366cf5
rafavzqz <rafavzqz@de73d896-90e3-4c87-98df-8390668dc151>
2015-11-06 12:12:59 +0000

    Removed old branch

--- branches/parallel_support/geopdes_base/inst/examples/geometry_files/ring_polar_map.m
+++
@@ -1,8 +0,0 @@
-% RING_POLAR_MAP: parameterization of a ring using polar coordinates.
-
-function F = ring_polar_map (pts)
-  u = pts(1,:); v = pts(2,:);
-  F(1,:) = (u+1) .* cos (pi.*v/2);
-  F(2,:) = (u+1) .* sin (pi.*v/2);
-end
-
--- branches/parallel_support/geopdes_base/pre_install.m
+++
@@ -1,4 +0,0 @@
-function pre_install (desc)
-  dir =  (fileparts (mfilename ("fullpath")));
-  putenv ("GEOPDES_INCLUDE_DIR", [ "-I" canonicalize_file_name(dir) filesep() "inst" filesep() "include"]);
-endfunction
