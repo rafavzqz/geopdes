6b699a11be6fe0c86d7c254c6b27d669c68b5cb3
vazquez <vazquez@imati.cnr.it>
2015-11-24 11:11:55 +0100

    Prova

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE HTML>
+<!--
+	Eleganta by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Lions-Magenes Days</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="viewport" content="width=1040" />
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" type="text/css" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+
+		<div id="wrapper">
+
+			<!-- Header -->
+			<div id="header">
+				<div class="container">
+					
+					<!-- Logo -->
+					<div id="logo">
+						<h1><a href="#">Lions-Magenes Days</a></h1>
+						<span>Pavia, April 13-14, 2015</span>
+					</div>
+					
+				</div>
+			</div>
+			<!-- Header -->
+			
+			<div class="container">
+				<!-- Nav -->
+				
+				<?php
+				
+				include('menu.php');
+				
+				?>
+				
+				<!-- /Nav -->
+			</div>
+			
+			<div class="container">
+				<div id="main">
+					<!-- Banner -->
+					<div id="banner">
+						<a href="#" class="image full"><img src="images/pavia033.jpg" alt=""></a>
+					</div>
+					<!-- /Banner -->
+					<div class="row">
+					<div class="3u">
+					<img src="images/Lions.jpg" width="200" height="320" alt="" border="0">
+					</div>	
+					<div class="6u" align='justify'>
+						<section>
+							<!-- <header>
+								<h2>One Column</h2>
+								<span class="byline">augue praesent urna congue rutrum</span>
+							</header> -->
+							
+							
+						
+					
+					
+						
+							The second Lions-Magenes Days will take place in Pavia on April 13-14, 2015. 
+							This two days scientific meeting, organized by the IMATI &quot;Enrico 
+							Magenes&quot; (Istituto di Matematica Applicata e Tecnologie Informatiche 
+							Enrico Magenes), the  LJLL 
+							(Laboratoire Jacques-Louis Lions), and the Department of Mathematics, Universit&agrave; degli Studi di Pavia, follows the meeting that took place in 
+							Paris in December 2011. 
+							By means of this event the two Institutes (nowadays both titled 
+							with the name of the respective founder) 
+							aim at commemorating the most prolific scientific relationship and the profound 
+							friendship between their two 
+							respective founders (after whom the institutes are named). 
+							Focused on young researchers from the two institutes, 
+							the meeting will give 16 PhD students and young researchers from the two 
+							institutions the occasion to present their work. 
+							More information on the meeting will appear on the site later on.
+</section>
+				</div>
+				
+					
+					
+					
+					<div class="3u">
+					<img align='right' src="images/Magenes.jpg" width="200" height="320" alt="" border="0">
+					</div>
+			
+						
+					</div> 
+					
+				</div>
+			</div>
+	
+	
+	<?php
+	
+	include('footer.php');
+	
+	?>
+	
+		</div>
+
+		<!-- Copyright -->
+		<div id="copyright">
+			<div class="container">
+				Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
+			</div>
+		</div>
+
+	</body>
+</html>
---
+++ lions-magenes.php
@@ -0,0 +1,130 @@
+<!DOCTYPE HTML>
+<!--
+	Eleganta by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Lions-Magenes Days</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="viewport" content="width=1040" />
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" type="text/css" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+
+		<div id="wrapper">
+
+			<!-- Header -->
+			<div id="header">
+				<div class="container">
+					
+					<!-- Logo -->
+					<div id="logo">
+						<h1><a href="#">Lions-Magenes Days</a></h1>
+						<span>Pavia, April 13-14, 2015</span>
+					</div>
+					
+				</div>
+			</div>
+			<!-- Header -->
+			
+			<div class="container">
+				<!-- Nav -->
+				<?php
+				
+				include('menu.php');
+				
+				?>
+				<!-- /Nav -->
+			</div>
+			
+			<div class="container">
+				<div id="main">
+					<!-- Banner -->
+					<div id="banner">
+						<a href="#" class="image full"><img src="images/pavia033.jpg" alt=""></a>
+					</div>
+					<!-- /Banner -->
+					<div class="row">
+					 <div class="3u">
+					<img src="images/Lions.jpg" width="200" height="320" alt="" border="0"> 
+					</div>	
+	<div class="6u">
+	<section align="justify">
+	More than forty years ago, two institutions, the Laboratoire d'Analyse Num&eacute;rique in 
+	Paris and the Laboratorio di Analisi Numerica (later renamed Istituto di Analisi Numerica) 
+	in Pavia, were founded respectively by Jacques-Louis Lions and Enrico Magenes 
+	with the common objective of facing questions related to the analysis and 
+	the numerical solution of partial differential equations. Both institutions 
+	were later renamed after their respective founder: the Laboratoire d'Analyse 
+	Num&eacute;rique takes in 2002 the name Laboratoire Jacques-Louis Lions, while the 
+	Istituto di Matematica Applicata e Tecnologie Informatiche (resulting from 
+	the fusion of the Istituto di Analisi Numerica with two other CNR institutes,  
+	IAM and IAMI) saw in 2011, the name of Enrico Magenes added to its official 
+	denomination. The two founders, who  shared a most prolific scientific 
+	relation as well as a profound friendship, wrote a great number of pages 
+	that can be considered as fundamental in the history of applied analysis 
+	(both theoretical and numerical) of PDE's. Under their guidance the 
+	collaboration between the two laboratories developed along different 
+	generations and resulted in a vast production in the field with a great 
+	number of joint papers.<br><br>
+	<h2>Some links</h2>
+	<a href="https://www.ljll.math.upmc.fr/Journees-Lions-Magenes/lions-magenes-publis.html" target="_blank">
+ J.-L. Lions and E. Magenes joint papers</a>
+<br>
+<a href="http://acm.smai.emath.fr/congres-jllions/" target="_blank">
+Congr&egrave;s Lions - Paris, July 1-5, 2002</a>
+<br>
+<a href="http://www.imati.cnr.it/magenes2011/" target="_blank">
+Conference in memory of Enrico Magenes - Pavia, November 2-4, 2011</a>
+<br>
+<a href="https://www.ljll.math.upmc.fr/Journees-Lions-Magenes" target="_blank">
+ Journ&eacute;es Lions-Magenes - Paris, December 14-15, 2011</a>
+<br>
+	
+	</section>
+	</div>
+	
+			
+					
+					
+					<div class="3u">
+					<img align='right' src="images/Magenes.jpg" width="200" height="320" alt="" border="0">
+					</div>
+			
+						
+					</div> 
+					
+				</div>
+			</div>
+<?php
+	
+	include('footer.php');
+	
+	?>
+		</div>
+
+		<!-- Copyright -->
+		<div id="copyright">
+			<div class="container">
+				Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
+			</div>
+		</div>
+
+	</body>
+</html>
