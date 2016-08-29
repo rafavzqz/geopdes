edf9969abf23de5f54ea3c89747af4151a83ff34
vazquez <vazquez@imati.cnr.it>
2016-08-29 14:31:41 +0200

    Some cleaning

--- geopdes/inst/collocation/ex_collocation.m
+++ geopdes/inst/collocation/ex_collocation.m
@@ -1,3 +1,4 @@
+% EX_COLLOCATION: 
 % This program implements stochastic collocation for the Poisson equation 
 % with homogeneous Dirichlet boundary conditions on a 2D/3D domain.
 % By Rafael Vazquez and Lorenzo Tamellini
@@ -20,9 +21,6 @@
 
 
 
-
-clear 
-
 %% ================================ problem and choice of discretization  ================================
 
 % problem_case = 1; % square 2D
@@ -32,16 +30,16 @@ problem_case = 2; % ring 2D
 
 switch problem_case
     
-    case 1    
+    case 1
         % Physical domain, defined as NURBS map given in a text file
-        problem_data.geo_name = 'geo_square.txt';        
+        problem_data.geo_name = 'geo_square.txt';
         % initial import of geometry
         geom_no_ref  = geo_load (problem_data.geo_name);
         problem_data.D = geom_no_ref.rdim;
         
         % Type of boundary conditions for each side of the domain
         problem_data.nmnn_sides   = [];
-        problem_data.drchlt_sides = [1 2 3 4];        
+        problem_data.drchlt_sides = [1 2 3 4];
         
         % Physical parameters
         problem_data.c_diff  = @(x, y) ones(size(x));
@@ -59,16 +57,16 @@ switch problem_case
         
     case 2
         % Physical domain, defined as NURBS map given in a text file
-        problem_data.geo_name = 'geo_ring.txt';        
+        problem_data.geo_name = 'geo_ring.txt';
         % initial import of geometry
         geom_no_ref  = geo_load (problem_data.geo_name);
-        geom_no_ref.nurbs = nrbdegelev(geom_no_ref.nurbs,[1 0]); % this way I get degree two in each direction        
+%         geom_no_ref.nurbs = nrbdegelev(geom_no_ref.nurbs,[1 0]); % this way I get degree two in each direction
         problem_data.D = geom_no_ref.rdim;
-
+        
         % Type of boundary conditions for each side of the domain
         problem_data.nmnn_sides   = [];
-        problem_data.drchlt_sides = [1 2 3 4];        
-
+        problem_data.drchlt_sides = [1 2 3 4];
+        
         % Physical parameters
         problem_data.c_diff  = @(x, y) ones(size(x));
         
@@ -80,22 +78,19 @@ switch problem_case
         % Exact solution (optional)
         problem_data.uex     = @(x, y) -(x.^2+y.^2-1).*(x.^2+y.^2.-4).*x.*y.^2;
         problem_data.graduex = @(x, y) cat (1,  reshape (-2*(x.*y).^2.*((x.^2+y.^2-1)+(x.^2+y.^2-4)) - (x.^2+y.^2-1).*(x.^2+y.^2-4).*y.^2, [1, size(x)]), ...
-                                        reshape ( -2*x.*y.^3.*((x.^2+y.^2-1)+(x.^2+y.^2-4)) -   2*x.*y.*(x.^2+y.^2-1).*(x.^2+y.^2-4), [1, size(x)]));        
-
-                                    
-                                    
+            reshape ( -2*x.*y.^3.*((x.^2+y.^2-1)+(x.^2+y.^2-4)) -   2*x.*y.*(x.^2+y.^2-1).*(x.^2+y.^2-4), [1, size(x)]));
+        
     case 3
         % Physical domain, defined as NURBS map given in a text file
-        problem_data.geo_name = 'geo_thick_ring.txt';        
+        problem_data.geo_name = 'geo_thick_ring.txt';
         % initial import of geometry
         geom_no_ref  = geo_load (problem_data.geo_name);
-        geom_no_ref.nurbs = nrbdegelev(geom_no_ref.nurbs,[1 0 1]); % this way I get degree two in each direction
-        problem_data.D = geom_no_ref.rdim;
-
+% %         geom_no_ref.nurbs = nrbdegelev(geom_no_ref.nurbs,[1 0 1]); % this way I get degree two in each direction
+% %         problem_data.D = geom_no_ref.rdim;
+        
         % Type of boundary conditions for each side of the domain
         problem_data.nmnn_sides   = [];
         problem_data.drchlt_sides = [ 1 2 3 4 5 6];
-
         
         % Physical parameters
         problem_data.c_diff  = @(x, y, z) ones(size(x));
@@ -104,79 +99,94 @@ switch problem_case
         problem_data.f = @(x,y,z) (20*x.^3.*y.^2-30*x.*y.^2+12*x.*y.^4+2*x.^5+30*x.*y.^4-10*x.^3-60*x.*y.^2+24*x.^3.*y.^2+8*x).*sin(pi*z) - pi^2*(x.^2+y.^2-1).*(x.^2+y.^2-4).*x.*y.^2.*sin(pi*z);
         problem_data.g = @(x, y, z, ind) zeros(size(x));
         problem_data.h = @(x, y, z, ind) zeros(size(x));
-
+        
         problem_data.uex = @(x,y,z) -(x.^2+y.^2-1).*(x.^2+y.^2-4).*x.*y.^2.*sin(pi*z);
         problem_data.dx_uex = @(x,y,z) -(4*x.^3-10*x+4*x.*y.^2).*x.*y.^2.*sin(pi*z) - (x.^4+y.^4-5*x.^2-5*y.^2+2*x.^2.*y.^2+4).*y.^2.*sin(pi*z);
         problem_data.dy_uex = @(x,y,z) -(4*y.^3-10*y+4*y.*x.^2).*x.*y.^2.*sin(pi*z) - (x.^4+y.^4-5*x.^2-5*y.^2+2*x.^2.*y.^2+4).*(2*y).*x.*sin(pi*z);
         problem_data.dz_uex = @(x,y,z) -(x.^2+y.^2-1).*(x.^2+y.^2-4).*x.*y.^2.*pi.*cos(pi*z);
         problem_data.graduex = @(x,y,z) cat (1,  reshape (problem_data.dx_uex(x,y,z), [1, size(x)]), ...
-                                                  reshape (problem_data.dy_uex(x,y,z), [1, size(x)]),...
-                                                  reshape (problem_data.dz_uex(x,y,z), [1, size(x)]));
-                                    
+            reshape (problem_data.dy_uex(x,y,z), [1, size(x)]),...
+            reshape (problem_data.dz_uex(x,y,z), [1, size(x)]));
+        
 end
-                   
-D = problem_data.D;                   
+
+if (isfield (geom_no_ref, 'nurbs'))
+  ndim = numel (geom_no_ref.nurbs.order);
+end
+
+% % D = problem_data.D;                   
                    
 % discretization parameters (p and h)
 
-method_data.degree     = 3*ones(1,D); % Degree of the splines, obtained by k-refinement of geometry, 
-                                      % e.g [3 3] or [3 3 3]
-method_data.n_sub      = 8*ones(1,D); % will divide each subinterval of the original knot span in n_sub many subinterval
-                                      % e.g. [9 9] or [9 9 9]
-                                      % i.e., we add nsub-1 knots in each interval of the original knotline.
-                                      % note that if the original geometry has even number of subintervals, all possible
-                                      % refinements with this strategy will have even subintervals
+method_data.degree = 3*ones(1,ndim); % Degree of the splines, obtained by k-refinement of geometry, 
+                                     % e.g [3 3] or [3 3 3]
+method_data.nsub   = 8*ones(1,ndim); % will divide each subinterval of the original knot span in nsub many subinterval
+                                     % e.g. [8 8] or [8 8 8]
+                                     % i.e., we add nsub-1 knots in each interval of the original knotline.
+                                     % note that if the original geometry has even number of subintervals, all possible
+                                     % refinements with this strategy will have even subintervals
 
 %% ================================  geometry ================================
 
 
-% make sure degree is identical in each direction
-if length(unique(geom_no_ref.nurbs.order))>1
-    error('different degrees in different dim')
-end
+% % % make sure degree is identical in each direction
+% % if length(unique(geom_no_ref.nurbs.order))>1
+% %     error('different degrees in different dim')
+% % end
 
-% compute how many degrees to elev. If 0 or less, something is wrong
+% Compute how many degrees to elev.
+% % compute how many degrees to elev. If 0 or less, something is wrong
 degelev  = max (method_data.degree - (geom_no_ref.nurbs.order-1), 0);
-if max(degelev)<=0,
-    error(strcat('max(degelev)=',num2str(max(degelev)),', stop! You need to fix the user set simulation degree'))
+if (any (degelev < 0))
+% %     error(strcat('max(degelev)=',num2str(max(degelev)),', stop! You need to fix the user set simulation degree'))
+    warning('The degree provided is lower than the degree of the original geometry')
+elseif (any (method_data.degree < 2))
+    error ('Collocation for the Laplacian requires at least C^1 continuity. Degree should be at least 2')
 end
-% otherwise, do degree elev
-nurbs_ptemp    = nrbdegelev (geom_no_ref.nurbs, degelev);
+nurbs_ptemp = nrbdegelev (geom_no_ref.nurbs, degelev);
 
 % next h-ref (so the overall procedure is k-ref). 
-new_knots = cell(1,problem_data.D);
+[rknots, zeta, new_knots] = kntrefine (nurbs_ptemp.knots, method_data.nsub-1, nurbs_ptemp.order-1, nurbs_ptemp.order-2);
 
-for d=1:D
-        
-    knotline = unique(nurbs_ptemp.knots{d});
-    nb_sub0 = length(knotline)-1;
-    NN = method_data.n_sub(d);
-    new_knots{d} = [];
-
-    for i = 1:nb_sub0
-        tmp_new = linspace(knotline(i),knotline(i+1),NN+1);
-        tmp_new(1)=[]; tmp_new(end)=[];
-        new_knots{d}(end+1:end+method_data.n_sub(d)-1) = tmp_new;
-        clear tmp_new
-    end
-    
-end
-
-% this is the final domain to use for computations
-nurbs = nrbkntins( nurbs_ptemp, new_knots);
+nurbs = nrbkntins (nurbs_ptemp, new_knots);
 geo_refined = geo_load (nurbs);
 
+% % for d = 1:ndim
+% %         
+% %     knotline = unique(nurbs_ptemp.knots{d});
+% %     nb_sub0 = length(knotline)-1;
+% %     NN = method_data.n_sub(d);
+% %     new_knots{d} = [];
+% % 
+% %     for i = 1:nb_sub0
+% %         tmp_new = linspace(knotline(i),knotline(i+1),NN+1);
+% %         tmp_new(1)=[]; tmp_new(end)=[];
+% %         new_knots{d}(end+1:end+method_data.n_sub(d)-1) = tmp_new;
+% %         clear tmp_new
+% %     end
+% %     
+% % end
+% % 
+% % % this is the final domain to use for computations
+% % nurbs = nrbkntins( nurbs_ptemp, new_knots);
+
 % these are the knot lines (with repetitions)
-knots=geo_refined.nurbs.knots;
+knots = geo_refined.nurbs.knots;
 
 
 %% ================================ Collocation ================================
 
+% Now we need to generate the collocation points. Since GeoPDEs is based on
+%  a mesh structure, we generate an auxiliary "mesh" with only one point 
+%  per element, using the midpoints between knots. 
+%  Although this is not the most efficient way to implement
+%  collocation, it allows us to use all the functionality in GeoPDEs, in
+%  particular the evaluation of basis functions, without further changes.
 
-% now we need to generate the collocation points. We define a sequence of coll_pts and create
-% a fictitious knot line so that we have 1 point per element. Then we build a mesh object
-% over this knot line and the collocation points. Then Geopdes treats them as quadrature points, 
-% but provides also a mean of evaluating the basis functions in those points, we go for it
+% % % now we need to generate the collocation points. We define a sequence of coll_pts and create
+% % % an auxiliary knot line so that we have 1 point per element. Then we build a mesh object
+% % % over this knot line and the collocation points. Then GeoPDEs treats them as quadrature points, 
+% % % but provides also a mean of evaluating the basis functions in those points, we go for it
 
 
 %pts_case = 1; % equispaced
@@ -187,10 +197,11 @@ pts_case = 2; % greville -------------------------------------------------------
                                                                                         % and also the Dir BC are treated eliminating 
                                                                                         % the DoFs (matrix columns) but not rows
 % pts_case = 3; % any external function prescribing points
-coll_pts = cell(1,D);
+coll_pts = cell(1,ndim);
 switch pts_case
     case 1
-        for d=1:D
+        for d=1:ndim
+% XXXX WHY 40? This should probably be in the input data.
             tmp = linspace(0,1,40); tmp(1)=[]; tmp(end)=[];
             coll_pts{d} = tmp; 
             clear tmp
@@ -199,47 +210,46 @@ switch pts_case
         %  aveknt(knot_line,k)  returns the averages of successive  k-1  knots. To comply
         % with the definition of greville as g_j = ( zeta_{j+1} + ... + zeta_{j+p})/p
         % for an open knot line with n+p+1 and degree p, we then pass as second argument p+1
-        for d=1:D
-            coll_pts{d} = aveknt(knots{d},method_data.degree(d)+1); 
+        for d = 1:ndim
+% %             coll_pts{d} = aveknt(knots{d},method_data.degree(d)+1); 
+            coll_pts{d} = aveknt (knots{d}, nurbs.order(d)); 
         end
     case 3
         % put your function here
         % coll_pts{d} = ...
 end
 
-
-
-% for each dim, we generate a fictitious knot line, so that there is one coll point per element.
-% we define this by taking as knot line [0 midpoint(coll-pt1, coll-pt2) midpoint(coll-pt2, coll-pt3) ... 1]
-brk = cell(1,D);
-for d = 1:D
+% % % For each dim, we generate an auxiliary knot line, so that there is one coll point per element.
+% % % we define this by taking as knot line [0 midpoint(coll-pt1, coll-pt2) midpoint(coll-pt2, coll-pt3) ... 1]
+brk = cell(1,ndim);
+for d = 1:ndim
     coll_pts{d} = coll_pts{d}(:)';
-    if numel(coll_pts{d}) > 1
+    if (numel(coll_pts{d}) > 1)
         brk{d} = [knots{d}(1), coll_pts{d}(1:end-1) + diff(coll_pts{d})/2, knots{d}(end)];
     else
         brk{d} = [knots{d}(1) knots{d}(end)];
     end
 end
 
-% here's the mesh with one coll pt per element
+% This is the auxiliary mesh with one coll pt per element
 coll_msh = msh_cartesian (brk, coll_pts, [], geo_refined, 'boundary', true,'der2',true);
 
-% now we generate the spline basis function set
-space = sp_nurbs( geo_refined.nurbs, coll_msh );
+% Now we generate the spline basis function set
+space = sp_nurbs (geo_refined.nurbs, coll_msh);
 
 
-% now we evaluate each spline in the collocation points and collect everything into the design matrix, i.e. a
+% Now we evaluate each spline in the collocation points and collect everything into the design matrix, i.e. a
 % matrix whose n-th row is the equation collocated in the n-th point. Remember that we have built coll_msh
 % to have 1 pt per element, so the number of coll pts is exactly the number of elements of coll_msh
 
 tot_nb_coll_pts = coll_msh.nel;
 nb_dofs = space.ndof;
-A = zeros(tot_nb_coll_pts,nb_dofs);
+% % A = zeros (tot_nb_coll_pts, nb_dofs);
+A = sparse (tot_nb_coll_pts, nb_dofs);
 
-% evaluate parameterization 
-coll_mesh_eval = msh_precompute(coll_msh);
-% evaluate basis fun 
-sp_evals  = sp_precompute(space, coll_mesh_eval, 'gradient', false, 'laplacian', true);
+% Evaluate parameterization and basis functions
+coll_mesh_eval = msh_precompute (coll_msh);
+sp_evals  = sp_precompute (space, coll_mesh_eval, 'gradient', false, 'laplacian', true);
 
 % sp_evals.shape_function_laplacian contains evaluations stored in a matrix of size
 % [msh.nqn x nsh_max x msh.nel double]      
@@ -247,7 +257,6 @@ sp_evals  = sp_precompute(space, coll_mesh_eval, 'gradient', false, 'laplacian',
 % the nonzero functions are included in the matrix connectivity, whose size is (nsh_max x msh.nel vector)         
 % so we need to unpack this information. To do this, we loop over the number of elements (i.e., over coll pts for this case),
 % detect the list of nonzero functions on that element and store the evaluations in A
-
 for iel = 1:coll_msh.nel %remember that although this formally spans elements, in practice it spans coll pts too
     
     %the nonzero funs on this element
@@ -256,21 +265,15 @@ for iel = 1:coll_msh.nel %remember that although this formally spans elements, i
     % since we have only 1 pt per element, shape_function_laplacian has in practice size
     % [1 x nonzerofun x nb_mesh_el] 
     A(iel,list_fun) = -sp_evals.shape_function_laplacians(1,:,iel); % sidenote ------------------------------> not this easy if instead we are solving -div{a grad u}=f
-    
 end
 
-
-% generate rhs. Recover coordinates of collocation points and evaluate forcing 
-x=cell(1,D);
-for d = 1:D
+% Generate rhs. Recover coordinates of collocation points and evaluate forcing 
+x = cell (1,ndim);
+for d = 1:ndim
     x{d} = reshape (coll_mesh_eval.geo_map(d,:,:), coll_mesh_eval.nqn*coll_msh.nel, 1);
 end
-switch D
-    case 2
-        rhs = problem_data.f(x{1},x{2});
-    case 3
-        rhs = problem_data.f(x{1},x{2},x{3});
-end
+rhs = problem_data.f(x{:});
+
 % remove DoF of boundary condition. Sidenote ----------------------------------------------------------> homogeneous Dir hardcoded here
 
 nb_boundaries = length(sp_evals.boundary);
@@ -285,27 +288,20 @@ internal_dofs = setdiff(1:sp_evals.ndof,boundary_dofs);
 AA = A'*A;
 rr = A'*rhs;
 
-sol = AA\rr;
-
-% finally, put together boundary and internal_dofs
-u_coll = zeros(sp_evals.ndof,1);
-
-u_coll(internal_dofs)=sol;
-
+u_coll = zeros (space.ndof,1);
+u_coll(internal_dofs) = AA\rr;
 
 
 %% ================================ Galerkin for comparison ================================
 
-% fix quad points, for good this time, and build mesh
-nquad      = 5*ones(1,D);     % Points for the Gaussian quadrature rule, e.g [5 5] or [5 5 5]
+% Even if we want to use the same space as before, we need to rebuild the
+% space object because now mesh is different
+nquad            = nurbs.order;     % Points for the Gaussian quadrature rule, equal to the order (degree + 1)
 [gal_qn, gal_qw] = msh_set_quad_nodes (knots, msh_gauss_nodes (nquad));
-gal_msh      = msh_cartesian (knots, gal_qn, gal_qw, geo_refined);
-  
-% Even if we want to use the same space as before, we need to rebuild the space object because now the mesh is
-% different
-gal_space = sp_nurbs(geo_refined.nurbs, gal_msh);
-% gal_space    = sp_bspline (knots, method_data.degree, gal_msh);
-
+gal_msh          = msh_cartesian (knots, gal_qn, gal_qw, geo_refined);
+gal_space = space.constructor (gal_msh);
+% % gal_space = sp_nurbs(geo_refined.nurbs, gal_msh);
+% % % gal_space    = sp_bspline (knots, method_data.degree, gal_msh);
 
 % Assemble the matrices
 gal_stiff_mat = op_gradu_gradv_tp (gal_space, gal_space, gal_msh, problem_data.c_diff);
@@ -332,7 +328,7 @@ u_gal(gal_int_dofs) = gal_stiff_mat(gal_int_dofs, gal_int_dofs) \ gal_rhs(gal_in
 
 
 % plot of solution (only for D=2)
-if D==2
+if ndim==2
     plot_pts = {linspace(0, 1, 40), linspace(0, 1, 40)};
     figure
     [eu, F] = sp_eval (u_coll, space, geo_refined, plot_pts);
@@ -352,7 +348,7 @@ end
 
 
 % compute errors of coll and gal. Create yet another mesh, which provides quad points for the error
-quad_err_nquad      = 9*ones(1,D);     % Points for the Gaussian quadrature rule, like [9 9] or [9 9 9]
+quad_err_nquad      = 9*ones(1,ndim);     % Points for the Gaussian quadrature rule, like [9 9] or [9 9 9]
 [quad_err_qn, quad_err_qw] = msh_set_quad_nodes (knots, msh_gauss_nodes(quad_err_nquad));
 quad_err_msh      = msh_cartesian (knots, quad_err_qn, quad_err_qw, geo_refined);
 
