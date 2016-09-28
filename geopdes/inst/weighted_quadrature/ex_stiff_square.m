88d945472510589e7d6fd5be8663960772892137
mattiatani <mattiatani@libero.it>
2016-09-28 11:37:29 +0200

    Add files via upload

---
+++ geopdes/inst/weighted_quadrature/ex_stiff_square.m
@@ -0,0 +1,34 @@
+
+nel=20;
+coeff  = @(x, y) ones(size(x));
+pmax = 5;
+
+for p = pmax:pmax
+
+degree     = [p p];       % Degree of the splines
+regularity = [p-1 p-1];       % Regularity of the splines
+nsub       = [nel nel];       % Number of subdivisions
+nquad      = [p+1 p+1];       % Points for the Gaussian quadrature rule
+
+geometry   = geo_load ('geo_square.txt');
+
+[knots, zeta] = kntrefine (geometry.nurbs.knots, nsub-1, degree, regularity);
+rule     = msh_gauss_nodes (nquad);
+[qn, qw] = msh_set_quad_nodes (zeta, rule);
+msh      = msh_cartesian (zeta, qn, qw, geometry);
+space    = sp_bspline (knots, degree, msh);
+
+tempo = tic;
+Stiff_new = Stiff_3D(msh, space, geometry, coeff);
+tempo_nostro = toc(tempo)
+
+tempo = tic;
+Stiff_geopdes = op_gradu_gradv_tp(space, space, msh, coeff);
+tempo_geopdes = toc(tempo)
+
+norm(Stiff_new - Stiff_geopdes,'fro')
+
+end
+
+
+
