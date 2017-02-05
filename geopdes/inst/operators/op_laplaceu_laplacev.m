7587107b4465273ede9d7184c0f8c617a1c54729
vazquez <vazquez@imati.cnr.it>
2017-02-05 13:30:48 +0100

    Vectorized version of the high order operators

--- geopdes/inst/operators/op_laplaceu_laplacev.m
+++ geopdes/inst/operators/op_laplaceu_laplacev.m
@@ -34,34 +34,49 @@
 %    You should have received a copy of the GNU General Public License
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function mat = op_laplaceu_laplacev (spu, spv, msh, coeff)
-   mat = spalloc (spv.ndof, spu.ndof, 1);
-   
-   laplaceu = reshape (spu.shape_function_hessians, spu.ncomp, [], msh.nqn, spu.nsh_max, msh.nel);
-   laplacev = reshape (spv.shape_function_hessians, spv.ncomp, [], msh.nqn, spv.nsh_max, msh.nel);
-   
-   ndir = size (laplaceu, 2);
- 
-   for iel = 1:msh.nel
-     if (all (msh.jacdet(:,iel)))
-       mat_loc = zeros (spv.nsh(iel), spu.nsh(iel));
-       for idof = 1:spv.nsh(iel)
-         ishg = reshape(laplacev(:,:,:,idof,iel),spv.ncomp * ndir, []);
-         for jdof = 1:spu.nsh(iel) 
-           jshg = reshape(laplaceu(:,:,:,jdof,iel),spu.ncomp * ndir, []);
-           % The cycle on the quadrature points is vectorized
-           %for inode = 1:msh.nqn
-           mat_loc(idof, jdof) = mat_loc(idof, jdof) + ...
-             sum (msh.jacdet(:,iel) .* msh.quad_weights(:, iel) .* ...
-             sum (ishg([1,1,4,4],:) .* jshg([1,4,1,4],:), 1).' .* coeff(:,iel));
-           %end
-         end
-       end
-       mat(spv.connectivity(:, iel), spu.connectivity(:, iel)) = ...
-         mat(spv.connectivity(:, iel), spu.connectivity(:, iel)) + mat_loc;
-     else
-       warning ('geopdes:jacdet_zero_at_quad_node',...
-           'op_laplaceu_laplacev: singular map in element number %d', iel)
-     end
-   end
+function varargout = op_laplaceu_laplacev (spu, spv, msh, coeff)
+
+  laplaceu = reshape (spu.shape_function_laplacians, spu.ncomp, msh.nqn, spu.nsh_max, msh.nel);
+  laplacev = reshape (spv.shape_function_laplacians, spv.ncomp, msh.nqn, spv.nsh_max, msh.nel);
+
+  rows = zeros (msh.nel * spu.nsh_max * spv.nsh_max, 1);
+  cols = zeros (msh.nel * spu.nsh_max * spv.nsh_max, 1);
+  values = zeros (msh.nel * spu.nsh_max * spv.nsh_max, 1);
+
+  jacdet_weights = msh.jacdet .* msh.quad_weights .* coeff;
+
+  ncounter = 0;
+  for iel = 1:msh.nel
+    if (all (msh.jacdet(:,iel)))
+      shpu_iel = reshape (laplaceu(:, :, 1:spu.nsh(iel), iel), spu.ncomp, msh.nqn, 1, spu.nsh(iel));
+%       shpu_iel = repmat (shpu_iel, [1,1,spv.nsh(iel),1]);
+      shpv_iel = reshape (laplacev(:, :, 1:spv.nsh(iel), iel), spv.ncomp, msh.nqn, spv.nsh(iel), 1);
+%       shpv_iel = repmat (shpv_iel, [1,1,1,spu.nsh(iel)]);
+
+      jacdet_iel = reshape (jacdet_weights(:,iel), [1,msh.nqn,1,1]);
+
+      jacdet_shpu = bsxfun (@times, jacdet_iel, shpu_iel);
+      tmp1 = sum (bsxfun (@times, jacdet_shpu, shpv_iel), 1);
+      values(ncounter+(1:spu.nsh(iel)*spv.nsh(iel))) = reshape (sum (tmp1, 2), spv.nsh(iel), spu.nsh(iel));
+
+      [rows_loc, cols_loc] = ndgrid (spv.connectivity(:,iel), spu.connectivity(:,iel));
+      rows(ncounter+(1:spu.nsh(iel)*spv.nsh(iel))) = rows_loc;
+      cols(ncounter+(1:spu.nsh(iel)*spv.nsh(iel))) = cols_loc;
+      ncounter = ncounter + spu.nsh(iel)*spv.nsh(iel);
+    else
+      warning ('geopdes:jacdet_zero_at_quad_node', 'op_u_v: singular map in element number %d', iel)
+    end
+  end
+
+  if (nargout == 1)
+    varargout{1} = sparse (rows(1:ncounter), cols(1:ncounter), ...
+                           values(1:ncounter), spv.ndof, spu.ndof);
+  elseif (nargout == 3)
+    varargout{1} = rows(1:ncounter);
+    varargout{2} = cols(1:ncounter);
+    varargout{3} = values(1:ncounter);
+  else
+    error ('op_u_v: wrong number of output arguments')
+  end
+
 end
