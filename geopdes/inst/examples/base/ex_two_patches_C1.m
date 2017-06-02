96b34298801cd2a4cbdf29f0a9dc3f75c2cf4dd4
vazquez <vazquez@imati.cnr.it>
2017-06-02 14:25:21 +0200

    Building the C1 example

---
+++ geopdes/inst/examples/base/ex_two_patches_C1.m
@@ -0,0 +1,105 @@
+% EX_LAPLACE_LSHAPED_MP: solve the Poisson problem in the multipatch L-shaped domain with a B-spline discretization.
+
+% 1) PHYSICAL DATA OF THE PROBLEM
+clear problem_data  
+% Physical domain, defined as NURBS map given in a text file
+nrb(1) = nrb4surf ([0 0], [-1 0], [0 1], [-1 1]);
+nrb(2) = nrb4surf ([0 0], [1 0], [0 1], [1 1]);
+
+problem_data.geo_name = nrb;
+
+% Type of boundary conditions for each side of the domain
+problem_data.nmnn_sides   = [];
+problem_data.drchlt_sides = [1 2 3 4 5 6];
+
+% Physical parameters
+problem_data.c_diff  = @(x, y) ones(size(x));
+
+% Source and boundary terms
+problem_data.f = @(x, y) exp(x).*((x.^2 + y.^2 - 1).*sin(x.*y) - 2*y.*cos(x.*y));
+problem_data.g = @(x, y, ind) test_Lshaped_mp_g_nmnn (x, y, ind);
+problem_data.h = @(x, y, ind) exp(x) .* sin (x.*y);
+
+% Exact solution (optional)
+problem_data.uex     = @(x, y) exp(x) .* sin (x.*y);
+problem_data.graduex = @(x, y) cat (1, ...
+               reshape (exp(x).*(sin(x.*y) + y.*cos(x.*y)), [1, size(x)]), ...
+               reshape (exp(x).*x.*cos(x.*y), [1, size(x)]));
+
+% 2) CHOICE OF THE DISCRETIZATION PARAMETERS
+clear method_data
+method_data.degree     = [3 3];       % Degree of the splines
+method_data.regularity = [2 2];       % Regularity of the splines
+method_data.nsub       = [9 9];       % Number of subdivisions
+method_data.nquad      = [4 4];       % Points for the Gaussian quadrature rule
+
+% 3) CALL TO THE SOLVER
+[geometry, msh, space, u] = mp_solve_laplace_C1 (problem_data, method_data);
+
+% 4) POST-PROCESSING
+% EXPORT TO PARAVIEW
+output_file = 'Lshaped_mp_BSP_Deg3_Reg2_Sub9';
+
+vtk_pts = {linspace(0, 1, 20), linspace(0, 1, 20)};
+fprintf ('The result is saved in the file %s.pvd \n \n', output_file);
+sp_to_vtk (u, space, geometry, vtk_pts, output_file, 'u')
+
+figure
+sp_plot_solution (u, space, geometry, vtk_pts)
+
+% COMPARISON WITH THE EXACT SOLUTION
+[error_h1, error_l2] = sp_h1_error (space, msh, u, problem_data.uex, problem_data.graduex)
+
+%!demo
+%! ex_laplace_Lshaped_mp
+
+%!test
+%! problem_data.geo_name = 'geo_Lshaped_mp.txt';
+%! problem_data.nmnn_sides   = [3 4 5 6];
+%! problem_data.drchlt_sides = [1 2];
+%! problem_data.c_diff  = @(x, y) ones(size(x));
+%! problem_data.f = @(x, y) exp(x).*((x.^2 + y.^2 - 1).*sin(x.*y) - 2*y.*cos(x.*y));
+%! problem_data.g = @(x, y, ind) test_Lshaped_mp_g_nmnn (x, y, ind);
+%! problem_data.h = @(x, y, ind) exp(x) .* sin (x.*y);
+%! problem_data.uex     = @(x, y) exp(x) .* sin (x.*y);
+%! problem_data.graduex = @(x, y) cat (1, ...
+%!                reshape ( exp(x).*(sin(x.*y) + y.*cos(x.*y)), [1, size(x)]), ...
+%!                reshape (exp(x).*x.*cos(x.*y), [1, size(x)]));
+%! method_data.degree     = [3 3];       % Degree of the splines
+%! method_data.regularity = [2 2];       % Regularity of the splines
+%! method_data.nsub       = [9 9];       % Number of subdivisions
+%! method_data.nquad      = [4 4];       % Points for the Gaussian quadrature rule
+%! [geometry, msh, space, u] = mp_solve_laplace (problem_data, method_data);
+%! [error_h1, error_l2] = sp_h1_error (space, msh, u, problem_data.uex, problem_data.graduex);
+%! assert (error_l2, 3.00642608570282e-07, 1e-16)
+%! assert (error_h1, 1.77524941085757e-05, 1e-16)
+%! assert (space.ndof, 408)
+%! for iptc = 1:space.boundary.npatch
+%!   patch = msh.boundary.patch_numbers(iptc);
+%!   side = msh.boundary.side_numbers(iptc);
+%!   assert (space.gnum{patch}(space.sp_patch{patch}.boundary(side).dofs)(:), space.boundary.dofs(space.boundary.gnum{iptc})(:))
+%! end
+%!
+%! problem_data.geo_name = 'geo_Lshaped_mp_b.txt';
+%! [geometry, msh, space, u] = mp_solve_laplace (problem_data, method_data);
+%! [error_h1, error_l2] = sp_h1_error (space, msh, u, problem_data.uex, problem_data.graduex);
+%! assert (error_l2, 3.00642608570282e-07, 1e-16)
+%! assert (error_h1, 1.77524941085757e-05, 1e-16)
+%! assert (space.ndof, 408)
+%! for iptc = 1:space.boundary.npatch
+%!   patch = msh.boundary.patch_numbers(iptc);
+%!   side = msh.boundary.side_numbers(iptc);
+%!   assert (space.gnum{patch}(space.sp_patch{patch}.boundary(side).dofs)(:), space.boundary.dofs(space.boundary.gnum{iptc})(:))
+%! end
+%!
+%! problem_data.geo_name = 'geo_Lshaped_mp_c.txt';
+%! [geometry, msh, space, u] = mp_solve_laplace (problem_data, method_data);
+%! [error_h1, error_l2] = sp_h1_error (space, msh, u, problem_data.uex, problem_data.graduex);
+%! assert (error_l2, 3.00642608570282e-07, 1e-16)
+%! assert (error_h1, 1.77524941085757e-05, 1e-16)
+%! assert (space.ndof, 408)
+%! for iptc = 1:space.boundary.npatch
+%!   patch = msh.boundary.patch_numbers(iptc);
+%!   side = msh.boundary.side_numbers(iptc);
+%!   assert (space.gnum{patch}(space.sp_patch{patch}.boundary(side).dofs)(:), space.boundary.dofs(space.boundary.gnum{iptc})(:))
+%! end
---
+++ geopdes/inst/multipatch/mp_solve_laplace_C1.m
@@ -0,0 +1,129 @@
+% MP_SOLVE_LAPLACE: solve the Laplacian problem in a multipatch geometry.
+%
+% Example to solve the diffusion problem
+%
+%    - div ( epsilon(x) grad (u)) = f    in Omega
+%                epsilon(x) du/dn = g    on Gamma_N
+%                               u = h    on Gamma_D
+%
+% where the domain \Omega is formed by several patches of the form F((0,1)^n).
+%
+% USAGE:
+%
+%  [geometry, msh, space, u] = 
+%          mp_solve_laplace (problem_data, method_data)
+%
+% INPUT:
+%
+%  problem_data: a structure with data of the problem. It contains the fields:
+%    - geo_name:     name of the file containing the geometry
+%    - nmnn_sides:   sides with Neumann boundary condition (may be empty)
+%    - drchlt_sides: sides with Dirichlet boundary condition
+%    - c_diff:       diffusion coefficient (epsilon in the equation)
+%    - f:            source term
+%    - g:            function for Neumann condition (if nmnn_sides is not empty)
+%    - h:            function for Dirichlet boundary condition
+%
+%  method_data : a structure with discretization data. Its fields are:
+%    - degree:     degree of the spline functions.
+%    - regularity: continuity of the spline functions.
+%    - nsub:       number of subelements with respect to the geometry mesh 
+%                   (nsub=1 leaves the mesh unchanged)
+%    - nquad:      number of points for Gaussian quadrature rule
+%
+% OUTPUT:
+%
+%  geometry: array of geometry structures (see geo_load)
+%  msh:      multipatch mesh, consisting of several Cartesian meshes (see msh_multipatch)
+%  space:    multipatch space, formed by several tensor product spaces plus the connectivity (see sp_multipatch)
+%  u:        the computed degrees of freedom
+%
+% Copyright (C) 2009, 2010 Carlo de Falco
+% Copyright (C) 2010, 2011, 2013, 2015 Rafael Vazquez
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 2 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+function [geometry, msh, space, u] = ...
+              mp_solve_laplace_C1 (problem_data, method_data)
+
+% Extract the fields from the data structures into local variables
+data_names = fieldnames (problem_data);
+for iopt  = 1:numel (data_names)
+  eval ([data_names{iopt} '= problem_data.(data_names{iopt});']);
+end
+data_names = fieldnames (method_data);
+for iopt  = 1:numel (data_names)
+  eval ([data_names{iopt} '= method_data.(data_names{iopt});']);
+end
+
+% Construct geometry structure, and information for interfaces and boundaries
+[geometry, boundaries, interfaces, ~, boundary_interfaces] = mp_geo_load (geo_name);
+npatch = numel (geometry);
+
+msh = cell (1, npatch); 
+sp = cell (1, npatch);
+for iptc = 1:npatch
+
+% Define the refined mesh, with tensor product structure
+  [knots{iptc}, zeta{iptc}] = ...
+         kntrefine (geometry(iptc).nurbs.knots, nsub-1, degree, regularity);
+
+% Compute the quadrature rule
+  rule      = msh_gauss_nodes (nquad);
+  [qn, qw]  = msh_set_quad_nodes (zeta{iptc}, rule);
+  msh{iptc} = msh_cartesian (zeta{iptc}, qn, qw, geometry(iptc));
+
+% Evaluate the discrete space basis functions in the quadrature points
+  sp{iptc} = sp_bspline (knots{iptc}, degree, msh{iptc});
+end
+
+msh = msh_multipatch (msh, boundaries);
+space = sp_multipatch (sp, msh, interfaces, boundary_interfaces);
+space_C1 = sp_multipatch_C1 (sp, msh, geometry, interfaces, boundary_interfaces);
+clear sp
+
+% Compute and assemble the matrices 
+stiff_mat = op_gradu_gradv_mp (space, space, msh, c_diff);
+rhs = op_f_v_mp (space, msh, f);
+
+% Apply Neumann boundary conditions
+Nbnd = cumsum ([0, boundaries.nsides]);
+for iref = nmnn_sides
+  iref_patch_list = Nbnd(iref)+1:Nbnd(iref+1);
+  gref = @(varargin) g(varargin{:},iref);
+  rhs_nmnn = op_f_v_mp (space.boundary, msh.boundary, gref, iref_patch_list);
+  rhs(space.boundary.dofs) = rhs(space.boundary.dofs) + rhs_nmnn;
+end
+
+% Apply Dirichlet boundary conditions
+u = zeros (space.ndof, 1);
+[u_drchlt, drchlt_dofs] = sp_drchlt_l2_proj (space, msh, h, drchlt_sides);
+u(drchlt_dofs) = u_drchlt;
+
+int_dofs = setdiff (1:space.ndof, drchlt_dofs);
+rhs(int_dofs) = rhs(int_dofs) - stiff_mat(int_dofs, drchlt_dofs)*u_drchlt;
+
+% Solve the linear system
+u(int_dofs) = stiff_mat(int_dofs, int_dofs) \ rhs(int_dofs);
+
+end
+
+%!demo
+%! ex_laplace_Lshaped_mp
+
+%!demo
+%! ex_laplace_cube_mp
+
+%!demo
+%! ex_laplace_thick_L_mp
