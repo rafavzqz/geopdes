ecbf5438abc15e193edc531730589add9d8d283b
vazquez <vazquez@imati.cnr.it>
2018-04-15 20:33:28 +0200

    Error functions for the stress

---
+++ geopdes/inst/space/@sp_vector/sp_stress_error.m
@@ -0,0 +1,50 @@
+% SP_H1_ERROR: Evaluate the error in H^1 norm.
+%
+%   [errh1, errl2, errh1s] = sp_h1_error (space, msh, u, uex, graduex)
+%
+% INPUT:
+%
+%    space:   object defining the space of discrete functions (see sp_vector)
+%    msh:     object defining the domain partition and the quadrature rule (see msh_cartesian)
+%    u:       vector of dof weights
+%    uex:     function handle to evaluate the exact solution
+%    graduex: function handle to evaluate the gradient of the exact solution
+%
+% OUTPUT:
+%
+%     errh1:  error in H^1 norm
+%     errl2:  error in L^2 norm
+%     errh1s: error in H^1 seminorm
+%
+% Copyright (C) 2010 Carlo de Falco
+% Copyright (C) 2011, 2015 Rafael Vazquez
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Octave; see the file COPYING.  If not, see
+% <http://www.gnu.org/licenses/>.
+
+function [err_stress] = sp_stress_error (space, msh, u, lambda_lame, mu_lame, graduex)
+% Graduex is the exact gradient. I compute the exact stress using it.
+
+  errh1s = 0;
+  for iel = 1:msh.nel_dir(1)
+    msh_col = msh_evaluate_col (msh, iel);
+    sp_col  = sp_evaluate_col (space, msh_col, 'value', false, 'gradient', true, 'divergence', true);
+    
+    err_h1s = sp_stress_error (sp_col, msh_col, u, lambda_lame, mu_lame, graduex);    
+    errh1s = errh1s + err_h1s.^2;
+  end
+  
+  err_stress = sqrt (errh1s);
+
+end
---
+++ geopdes/inst/space/sp_stress_error_mixed.m
@@ -0,0 +1,80 @@
+% SP_H1_ERROR: Evaluate the error in H^1 norm.
+%
+%   [errh1, errl2, errh1s, errh1_elem, errl2_elem, errh1s_elem] = sp_h1_error (space, msh, u, uex, graduex);
+%
+% INPUT:
+%
+%     space:    structure representing the space of discrete functions (see sp_scalar/sp_evaluate_col)
+%     msh:      structure containing the domain partition and the quadrature rule (see msh_cartesian/msh_evaluate_col)
+%     u:        vector of dof weights
+%     uex:      function handle to evaluate the exact solution
+%     graduex:  function handle to evaluate the gradient of the exact solution
+%
+% OUTPUT:
+%
+%     errh1:  error in H^1 norm
+%     errl2:  error in L^2 norm
+%     errh1s: error in H^1 seminorm
+%     errh1_elem:  error in H^1 norm, for each single element
+%     errl2_elem:  error in L^2 norm, for each single element
+%     errh1s_elem: error in H^1 seminorm, for each single element
+%
+% Copyright (C) 2010 Carlo de Falco
+% Copyright (C) 2011 Rafael Vazquez
+% Copyright (C) 2015 Eduardo M. Garau, Rafael Vazquez
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Octave; see the file COPYING.  If not, see
+% <http://www.gnu.org/licenses/>.
+
+function [err_stress, err_stress_elem] = sp_stress_error_mixed (sp, spp, msh, u, press, lambda_lame, mu_lame, graduex)
+
+  for idir = 1:msh.rdim
+    x{idir} = reshape (msh.geo_map(idir,:,:), msh.nqn*msh.nel, 1);
+  end
+  coeff_mu = reshape (mu_lame(x{:}), [1, 1, msh.nqn, msh.nel]);
+  coeff_lambda = reshape (lambda_lame(x{:}), [1, 1, msh.nqn, msh.nel]);
+  
+  grad_valu = sp_eval_msh (u, sp, msh, 'gradient');
+  grad_valu = reshape (grad_valu, sp.ncomp, msh.rdim, msh.nqn, msh.nel);
+  gradt_valu = permute (grad_valu, [2 1 3 4]);
+  press_valu = sp_eval_msh (press, spp, msh, 'value');
+
+  stress1 = bsxfun (@times, coeff_mu, (grad_valu + gradt_valu));
+  for ii = 1:msh.rdim
+    stress2(ii,ii,:,:) = press_valu;
+  end
+  stress_valu = stress1 + stress2;
+
+  grad_valex  = reshape (feval (graduex, x{:}), sp.ncomp, msh.rdim, msh.nqn, msh.nel);
+  gradt_valex = permute (grad_valex, [2 1 3 4]);
+  div_valex = 0;
+  for ii = 1:msh.rdim
+    div_valex = div_valex + grad_valex(ii,ii,:,:);
+  end
+
+  stress1 = bsxfun (@times, coeff_mu, (grad_valex + gradt_valex));
+  div_valex = coeff_lambda .* div_valex;
+  for ii = 1:msh.rdim
+    stress2(ii,ii,:,:) = div_valex;
+  end
+  stress_valex = stress1 + stress2;
+
+  w = msh.quad_weights .* msh.jacdet;
+
+  err_stress_elem = sum (reshape (sum (sum ((stress_valu - stress_valex).^2, 1), 2), [msh.nqn, msh.nel]) .* w);
+  err_stress = sqrt (sum (err_stress_elem));
+
+  err_stress_elem = sqrt (err_stress_elem);
+  
+end
