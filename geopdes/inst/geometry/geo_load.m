d859a520115f0f17e7b3b8efe3f29e5913987b1e
ochanon <ondine.chanon@epfl.ch>
2019-01-27 22:41:20 +0100

    gismo_map moved to geopdes_hierarchical

--- geopdes/inst/geometry/geo_load.m
+++ geopdes/inst/geometry/geo_load.m
@@ -337,62 +337,6 @@ function varargout = boundary_map_der (map, map_der, iside, pts)
 
 end
 
-% These two functions are used to compute mappings from G+smo geometries
-function varargout = gismo_map (pts, in, der)
-  if ( iscell (pts) ) 
-      ndim = length (pts);
-      npts = prod (cellfun (@length,pts));
-      pts = cartesian_product_from_cell (pts);
-  else
-      [ndim, npts] = size (pts);
-  end
-  
-  if (der == 0 || nargout > 1)
-    F = in.eval(pts); % dimension rdim x npts
-    varargout{1} = F;
-  end
-  if (der == 1 || nargout > 2)
-    % g+smo dim: rdim x (ndim x npts)
-    % geopdes dim: rdim x ndim x npts
-    jac = reshape (in.jacobian(pts), [], ndim, npts); 
-    if nargout == 1
-        varargout{1} = jac;
-    else
-        varargout{2} = jac;
-    end
-  end
-  if (der == 2)
-    rdim = in.geoDim;
-    % g+smo dim: rdim, (ndim x ndim) x npts
-    % geopdes dim rdim x ndim x ndim x npts
-    hess = zeros (rdim, ndim, ndim, npts);
-    for dir = 1:rdim
-      hess(dir,:,:,:) = reshape (in.hess(pts, dir), ndim, ndim, npts);
-    end
-    if nargout == 1
-      varargout{1} = hess;
-    elseif nargout == 3
-      varargout{3} = hess;
-    end
-  end
-end
-
-function pts_aux = cartesian_product_from_cell (pts)
-  % create cartesian product points from cell information
-  s = cellfun (@length, pts);
-  s_cell = cell (length(s), 1);
-  for ii = 1:length(s)
-      s_cell{ii} = 1:s(ii);
-  end
-  x = cell (1, numel (s_cell));
-  [x{:}] = ndgrid (s_cell{:});
-  pts_aux = [];
-  for ii = 1:length(s)
-      pts_aux = [pts_aux ; reshape( pts{ii}(x{ii}), 1, [] )];
-  end
-end
-
-
 %!shared g1,g2,x
 %!test
 %! g1 = geo_load ('geo_ring.txt');
