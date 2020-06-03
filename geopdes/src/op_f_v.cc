6de9b0bef6b40b2fb7f341e4ce02013e8704d095
Carlo de Falco <carlo.defalco@polimi.it>
2020-06-03 23:03:18 +0200

    allow complex coefficients

--- geopdes/src/op_f_v.cc
+++ geopdes/src/op_f_v.cc
@@ -19,6 +19,53 @@
 
 #include "geopdes.h"
 
+template<class ARRAY, class NUMBER>
+void op_f_v_worker (const geopdes_mesh &msh, const geopdes_space &sp,
+                    const ARRAY &coeff, NUMBER *matptr)
+{
+
+  const octave_idx_type
+    nel = msh.nel (),
+    ncomp = sp.ncomp (), 
+    nqn = msh.nqn (),
+    nsh = sp.nsh_max ();
+
+  double shp[nsh][nqn][ncomp];
+  octave_idx_type conn[nsh];
+  double jacdet_weights[nqn];
+  
+  octave_idx_type iel, inode, idof, icmp;
+      
+  for (iel=0; iel < nel; iel++) 
+    if (msh.area (iel) > 0.0)
+      {              
+        for (inode = 0; inode < nqn; inode++)
+          jacdet_weights[inode] = msh.jacdet (inode, iel) *
+            msh.weights (inode, iel);
+            
+        sp.cache_element_shape_functions (iel, (double*)shp);
+        sp.cache_element_connectivity (iel, (octave_idx_type*)conn);
+            
+        for (idof = 0; idof < nsh; idof++) 
+          for (inode = 0; inode < nqn; inode++)
+            if (msh.weights (inode, iel) > 0.0)
+              {
+
+                NUMBER s = 0.0;
+
+                for (icmp = 0; icmp < ncomp; icmp++)
+                  s += shp[idof][inode][icmp] * coeff (icmp, inode, iel);
+
+                matptr[(conn[idof])-1] += jacdet_weights[inode] * s;
+		  
+              } // end for idof, for inode, if weight > 0
+      }  // end for iel, if area > 0 
+    else 
+      {
+        warning_with_id ("geopdes:zero_measure_element", "op_f_v: element %lld has 0 area (or volume)", static_cast<long long int> (iel));
+      }
+}
+
 DEFUN_DLD(op_f_v, args, nargout,"\n\
 OP_F_V: assemble the right-hand side vector r = [r(i)], with  r(i) = (f, v_i). \n\
 \n\
@@ -43,53 +90,27 @@ OUTPUT:\n\
     
   geopdes_mesh  msh (args(1).scalar_map_value ());
   geopdes_space sp  (args(0).scalar_map_value (), msh);
-
-  const octave_idx_type nel = msh.nel (), ncomp = sp.ncomp (), 
-    nqn = msh.nqn (), nsh = sp.nsh_max ();
-
-  double jacdet_weights[nqn];
-
-  dim_vector    idx (ncomp, nqn, nel);
-  Array<double> coeff = args(2).array_value ().reshape (idx);
-
-  double shp[nsh][nqn][ncomp];
-  octave_idx_type conn[nsh];
-  ColumnVector  mat (sp.ndof (), 0.0);
-  double *matptr = mat.fortran_vec ();
-
-  octave_idx_type iel, inode, idof, icmp;
-      
-  for (iel=0; iel < nel; iel++) 
-    if (msh.area (iel) > 0.0)
-      {              
-        for (inode = 0; inode < nqn; inode++)
-          jacdet_weights[inode] = msh.jacdet (inode, iel) *
-            msh.weights (inode, iel);
-            
-        sp.cache_element_shape_functions (iel, (double*)shp);
-        sp.cache_element_connectivity (iel, (octave_idx_type*)conn);
-            
-        for (idof = 0; idof < nsh; idof++) 
-          for (inode = 0; inode < nqn; inode++)
-            if (msh.weights (inode, iel) > 0.0)
-              {
-
-                double s = 0.0;
-
-                for (icmp = 0; icmp < ncomp; icmp++)
-                  s += shp[idof][inode][icmp] * coeff (icmp, inode, iel);
-
-                matptr[(conn[idof])-1] += jacdet_weights[inode] * s;
-		  
-              } // end for idof, for inode, if weight > 0
-      }  // end for iel, if area > 0 
-    else 
-      {
-        warning_with_id ("geopdes:zero_measure_element", "op_f_v: element %lld has 0 area (or volume)", static_cast<long long int> (iel));
-      }
-      
-  retval(0) = octave_value (mat);
+  dim_vector    idx (sp.ncomp (), msh.nqn (), msh.nel ());
   
+
+  if (args(2).iscomplex ())
+    {
+      Array<Complex> coeff = args(2).complex_array_value ().reshape (idx);
+      ComplexColumnVector mat (sp.ndof (), 0.0);
+      Complex *matptr = mat.fortran_vec ();
+      op_f_v_worker (msh, sp, coeff, matptr);
+      retval(0) = octave_value (mat);
+    }
+  else
+    {
+      Array<double> coeff = args(2).array_value ().reshape (idx);
+      ColumnVector  mat (sp.ndof (), 0.0);
+      double *matptr = mat.fortran_vec ();
+      op_f_v_worker (msh, sp, coeff, matptr);
+      retval(0) = octave_value (mat);
+    }
+ 
+
   return retval;
 }
 
