d0146ac29d699d47b0da7c63164f38156c42e670
Andrea Farahat <andrea.farahat@ricam.oeaw.ac.at>
2022-05-12 17:44:32 +0200

    Restored the status of files after tests

--- geopdes/inst/space/@sp_scalar/sp_l2_error.m
+++ geopdes/inst/space/@sp_scalar/sp_l2_error.m
@@ -40,7 +40,7 @@ function errl2 = sp_l2_error (space, msh, u, uex)
   
   for iel = 1:msh.nel_dir(1)
     msh_col = msh_evaluate_col (msh, iel);
-    sp_col  = sp_evaluate_col (space, msh_col, 'value', true, 'gradient', false, 'laplacian', true);
+    sp_col  = sp_evaluate_col (space, msh_col, 'value', true, 'gradient', false);
     
     errl2 = errl2 + (sp_l2_error (sp_col, msh_col, u, uex)).^2;
   end
