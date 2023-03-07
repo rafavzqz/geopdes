57ac25a2ccb5d7e0bcd5fc8c1bc21b0399608098
vazquez <vazquez@imati.cnr.it>
2023-03-07 11:22:51 +0100

    Added new operators

---
+++ geopdes/inst/multipatch/@sp_multipatch/op_f_gradv_mp.m
@@ -0,0 +1,51 @@
+% OP_F_GRADV_MP: assemble the right-hand side vector r = [r(i)], with  r(i) = (f, grad v_i), in a multipatch domain.
+%
+%   rhs = op_f_gradv_mp (spv, msh, coeff, [patches]);
+%
+% INPUT:
+%     
+%   spv:     object representing the function space (see sp_multipatch)
+%   msh:     object defining the domain partition and the quadrature rule (see msh_multipatch)
+%   coeff:   function handle to compute the source function
+%   patches: list of patches where the integrals have to be computed. By default, all patches are selected.
+%
+% OUTPUT:
+%
+%   rhs: assembled right-hand side
+% 
+% Copyright (C) 2015, 2023 Rafael Vazquez
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+function rhs = op_f_gradv_mp (space, msh, coeff, patch_list)
+
+  if (nargin < 4)
+    patch_list = 1:msh.npatch;
+  end
+
+  if (space.npatch ~= msh.npatch)
+    error ('op_f_gradv_mp: the number of patches does not coincide')
+  end
+  
+  rhs = zeros (space.ndof, 1);
+  for iptc = patch_list
+    rhs_loc = op_f_gradv_tp (space.sp_patch{iptc}, msh.msh_patch{iptc}, coeff);
+    
+    if (~isempty (space.dofs_ornt))
+      rhs_loc = space.dofs_ornt{iptc}(:) .* rhs_loc(:);
+    end
+    rhs(space.gnum{iptc}) = rhs(space.gnum{iptc}) + rhs_loc;
+  end
+
+end
